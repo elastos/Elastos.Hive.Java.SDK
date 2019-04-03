1916dc8417cb617d153aef85c7363727b784244f
wanli <zhaowanli@elastos.org>
2019-04-03 11:14:35 +0800

    1. Implement the Hive's interfaces; 2. Update the test cases.

--- app/src/main/java/org/elastos/hive/Node.java
+++ app/src/main/java/org/elastos/hive/Node.java
@@ -31,96 +31,20 @@ public class Node {
 		return NodeCmd.execute(NodeCmd.UID_INFO, arg);
 	}
 
-	public static JSONObject pinAdd(String hash) {
-		if (hash == null || hash.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
+	public static JSONObject uidLogin(String uid, String hash) {
+		if (uid == null || uid.isEmpty() || hash == null || hash.isEmpty()) {
+			throw new IllegalArgumentException("Invalid uid or home hash.");
 		}
 
-		String args = "arg=" + hash;
-		args += "&recursive=true";
-
-		return NodeCmd.execute(NodeCmd.PIN_ADD, args);
-	}
-
-	public static JSONObject pinAdd(String hash, boolean recursive, boolean progress) {
-		if (hash == null || hash.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
-		}
-
-		StringBuilder sb = new StringBuilder("arg=" + hash);
-		if (recursive) {
-			sb.append("&recursive=true");
+		String arg = "uid="+uid;
+		if (!hash.contains(NodeCmd.PREFIX)) {
+			arg += "&hash="+ NodeCmd.PREFIX + hash;
 		}
 		else {
-			sb.append("&recursive=false");
+			arg += "&hash="+hash;
 		}
 
-		if (progress) {
-			sb.append("&progress=true");
-		}
-		else {
-			sb.append("&progress=false");
-		}
-
-		return NodeCmd.execute(NodeCmd.PIN_ADD, sb.toString());
-	}
-
-	public static JSONObject pinLs(String hash) {
-		if (hash == null || hash.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
-		}
-
-		String args = "arg=" + hash;
-		args += "&type=all";
-
-		return NodeCmd.execute(NodeCmd.PIN_LS, args);
-	}
-
-	public static JSONObject pinLs(String arg, String type, boolean quiet) {
-		if (arg == null || arg.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
-		}
-
-		StringBuilder sb = new StringBuilder("arg=" + arg);
-		if (type != null && !type.isEmpty()) {
-			sb.append("&type=").append(type);
-		}
-
-		if (quiet) {
-			sb.append("&quiet=true");
-		}
-		else {
-			sb.append("&quiet=false");
-		}
-
-		return NodeCmd.execute(NodeCmd.PIN_LS, sb.toString());
-	}
-
-	public static JSONObject pinRm(String hash) {
-		if (hash == null || hash.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
-		}
-
-		String args = "arg=" + hash;
-		args += "&recursive=true";
-
-		return NodeCmd.execute(NodeCmd.PIN_RM, args);
-	}
-
-	public static JSONObject pinRm(String arg, boolean recursive) {
-		if (arg == null || arg.isEmpty()) {
-			throw new IllegalArgumentException("Invalid arg.");
-		}
-
-		StringBuilder sb = new StringBuilder("arg=" + arg);
-		if (recursive) {
-			sb.append("&recursive=true");
-		}
-		else {
-			sb.append("&recursive=false");
-		}
-
-		return NodeCmd.execute(NodeCmd.PIN_RM, sb.toString());
+		return NodeCmd.execute(NodeCmd.UID_LOGIN, arg);
 	}
 
 	public static JSONObject fileAdd(String path) {
@@ -128,15 +52,10 @@ public class Node {
 			throw new IllegalArgumentException("Invalid path.");
 		}
 
-		Map<String, String> params = new HashMap<>();
-		params.put("recursive", "false");
-		params.put("hidden", "false");
-		params.put("pin", "true");
-
-		return NodeCmd.execute(NodeCmd.FILE_ADD, path, null, params);
+		return fileAdd(path, false, false, true);
 	}
 
-	public static JSONObject fileAdd(String path, boolean recursive, boolean hidden, boolean pin) {
+	private static JSONObject fileAdd(String path, boolean recursive, boolean hidden, boolean pin) {
 		if (path == null || path.isEmpty()) {
 			throw new IllegalArgumentException("Invalid path.");
 		}
@@ -166,7 +85,7 @@ public class Node {
 		return NodeCmd.execute(NodeCmd.FILE_ADD, path, null, params);
 	}
 
-	public static String fileGet(String hash) {
+	public static byte[] fileGet(String hash) {
 		if (hash == null || hash.isEmpty()) {
 			throw new IllegalArgumentException("Invalid args.");
 		}
@@ -175,14 +94,13 @@ public class Node {
 		return NodeCmd.executeEx(NodeCmd.FILE_GET, args);
 	}
 
-	public static String fileGet(String hash, String output, boolean archive
+	public static byte[] fileGet(String hash, boolean archive
 			, boolean compress, int compressionLevel) {
 		if (hash == null || hash.isEmpty() || compressionLevel > 9 || compressionLevel < 1) {
 			throw new IllegalArgumentException("Invalid args.");
 		}
 
 		String args = "arg=" + hash;
-		args += "&output=" + output;
 		if (archive) {
 			args += "&archive=true";
 		}
@@ -202,7 +120,7 @@ public class Node {
 		return NodeCmd.executeEx(NodeCmd.FILE_GET, args);
 	}
 
-	public static String fileCat(String hash) {
+	public static byte[] fileCat(String hash) {
 		if (hash == null || hash.isEmpty()) {
 			throw new IllegalArgumentException("Invalid args.");
 		}
@@ -221,14 +139,15 @@ public class Node {
 	}
 
 	public static void filesCp(String uid, String source, String dest) {
-		if (uid == null || uid.isEmpty()) {
+		if (uid == null || uid.isEmpty() || source == null || source.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
 
 		StringBuilder sb = new StringBuilder("uid=");
 		sb.append(uid);
-		if (source != null && !source.isEmpty()) {
-			sb.append("&source=").append(source);
+
+		if (!source.contains(NodeCmd.PREFIX)) {
+			sb.append("&source=").append(NodeCmd.PREFIX + source);
 		}
 
 		if (dest != null && !dest.isEmpty()) {
@@ -238,7 +157,11 @@ public class Node {
 		NodeCmd.execute(NodeCmd.FILES_CP, sb.toString());
 	}
 
-	public static void filesFlush(String uid, String path) {
+	public static void filesFlush(String uid) {
+		filesFlush(uid, "/");
+	}
+
+	private static void filesFlush(String uid, String path) {
 		if (uid == null || uid.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
@@ -272,7 +195,7 @@ public class Node {
 		return NodeCmd.execute(NodeCmd.FILES_LS, sb.toString());
 	}
 
-	public static JSONObject filesMkdir(String uid, String path, boolean parents) {
+	public static void filesMkdir(String uid, String path, boolean parents) {
 		if (uid == null || uid.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
@@ -288,10 +211,10 @@ public class Node {
 			sb.append("false");
 		}
 
-		return NodeCmd.execute(NodeCmd.FILES_MKDIR, sb.toString());
+		NodeCmd.execute(NodeCmd.FILES_MKDIR, sb.toString());
 	}
 
-	public static JSONObject filesMv(String uid, String source, String dest) {
+	public static void filesMv(String uid, String source, String dest) {
 		if (uid == null || uid.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
@@ -300,10 +223,10 @@ public class Node {
 		args += "&source=" + source;
 		args += "&dest=" + dest;
 
-		return NodeCmd.execute(NodeCmd.FILES_MV, args);
+		NodeCmd.execute(NodeCmd.FILES_MV, args);
 	}
 
-	public static String filesRead(String uid, String path) {
+	public static byte[] filesRead(String uid, String path) {
 		if (uid == null || uid.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
@@ -314,7 +237,7 @@ public class Node {
 		return NodeCmd.executeEx(NodeCmd.FILES_READ, args);
 	}
 
-	public static String filesRead(String uid, String path, int offset, int count) {
+	public static byte[] filesRead(String uid, String path, int offset, int count) {
 		if (uid == null || uid.isEmpty() || count < 0) {
 			throw new IllegalArgumentException("Invalid uid.");
 		}
@@ -327,39 +250,32 @@ public class Node {
 		return NodeCmd.executeEx(NodeCmd.FILES_READ, args);
 	}
 
-	public static void filesRm(String uid, String path, boolean recursive) {
+	public static void filesRm(String uid, String path) {
 		if (uid == null || uid.isEmpty() || path == null || path.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid or path");
 		}
 
 		String args = "uid=" + uid;
 		args += "&path=" + path;
-		if (recursive) {
-			args += "&recursive=true";
-		}
-		else {
-			args += "&recursive=false";
-		}
+		args += "&recursive=true";
 
 		NodeCmd.execute(NodeCmd.FILES_RM, args);
 	}
 
 	public static JSONObject filesStat(String uid, String path) {
-		if (uid == null || uid.isEmpty() || path == null || path.isEmpty()) {
-			throw new IllegalArgumentException("Invalid uid or path");
-		}
-
-		String args = "uid=" + uid;
-		args += "&path=" + path;
-		return NodeCmd.execute(NodeCmd.FILES_STAT, args);
+		return filesStat(uid, path, null, null, null);
 	}
 
-	public static JSONObject filesStat(String uid, String path, String format, String hash, String withLocal) {
-		if (uid == null || uid.isEmpty() || path == null || path.isEmpty()) {
-			throw new IllegalArgumentException("Invalid uid or path");
+	private static JSONObject filesStat(String uid, String path, String format, String hash, String withLocal) {
+		if (uid == null || uid.isEmpty()) {
+			throw new IllegalArgumentException("Invalid uid");
 		}
 
 		String args = "uid=" + uid;
+		if (path == null || path.isEmpty()) {
+			path = "/";
+		}
+
 		args += "&path=" + path;
 		if (format != null && !format.isEmpty()) {
 			args += "&format=" + format;
@@ -376,18 +292,11 @@ public class Node {
 		return NodeCmd.execute(NodeCmd.FILES_STAT, args);
 	}
 
-	public static JSONObject filesWrite(String uid, byte[] data, String hivePath) {
-		if (uid == null || uid.isEmpty() || data == null || data.length <= 0) {
-			throw new IllegalArgumentException("Invalid uid or data");
-		}
-
-		String args = "?uid=" + uid;
-		args += "&path=" + hivePath;
-		args += "&create=true";
-		return NodeCmd.execute(NodeCmd.FILES_WRITE + args, null, data, null);
+	public static void filesWrite(String uid, byte[] data, String hivePath) {
+		filesWrite(uid, data, hivePath, 0, true, true, data.length);
 	}
 
-	public static JSONObject filesWrite(String uid, byte[] data, String hivePath, int offset, boolean create,
+	public static void filesWrite(String uid, byte[] data, String hivePath, int offset, boolean create,
 	                             boolean truncate, int count) {
 		if (uid == null || uid.isEmpty() || data == null || data.length <= 0) {
 			throw new IllegalArgumentException("Invalid uid or data");
@@ -411,10 +320,10 @@ public class Node {
 			args += "&truncate=false";
 		}
 
-		return NodeCmd.execute(NodeCmd.FILES_WRITE + args, null, data, null);
+		NodeCmd.execute(NodeCmd.FILES_WRITE + args, null, data, null);
 	}
 
-	public static JSONObject filesWrite(String uid, String filePath, String hivePath) {
+	public static void filesWrite(String uid, String filePath, String hivePath) {
 		if (uid == null || uid.isEmpty() || filePath == null || filePath.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid or filePath");
 		}
@@ -423,10 +332,10 @@ public class Node {
 		args += "&path=" + hivePath;
 		args += "&create=true";
 
-		return NodeCmd.execute(NodeCmd.FILES_WRITE + args, filePath, null, null);
+		NodeCmd.execute(NodeCmd.FILES_WRITE + args, filePath, null, null);
 	}
 
-	public static JSONObject filesWrite(String uid, String filePath, String hivePath, int offset, boolean create,
+	public static void filesWrite(String uid, String filePath, String hivePath, int offset, boolean create,
 	                             boolean truncate, int count) {
 		if (uid == null || uid.isEmpty() || filePath == null || filePath.isEmpty()) {
 			throw new IllegalArgumentException("Invalid uid or filePath");
@@ -450,21 +359,11 @@ public class Node {
 			args += "&truncate=false";
 		}
 
-		return NodeCmd.execute(NodeCmd.FILES_WRITE + args, filePath, null, null);
+		NodeCmd.execute(NodeCmd.FILES_WRITE + args, filePath, null, null);
 	}
 
-	public static JSONObject namePublish(String uid, String lifetime, String ipfsFile) {
-		if (uid == null || uid.isEmpty() || ipfsFile == null || ipfsFile.isEmpty()) {
-			throw new IllegalArgumentException("Invalid uid or ipfsFile");
-		}
-
-		String args = "uid=" + uid;
-		if (lifetime != null && !lifetime.isEmpty()) {
-			args += "&lifetime=" + lifetime;
-		}
-		args += "&path=" + ipfsFile;
-
-		return NodeCmd.execute(NodeCmd.NAME_PUBLISH, args);
+	public static JSONObject getVersion() {
+		return NodeCmd.execute(NodeCmd.VERSION, null);
 	}
 
 	public static JSONObject getVersion(boolean number, boolean commit, boolean repo, boolean all) {
@@ -498,9 +397,5 @@ public class Node {
 		}
 
 		return NodeCmd.execute(NodeCmd.VERSION, args);
-	}
-
-	public static JSONObject getVersion() {
-		return NodeCmd.execute(NodeCmd.VERSION, null);
 	}
 }
