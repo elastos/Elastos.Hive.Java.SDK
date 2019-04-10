809f4927512e436841717e65b6d89ba466b785de
stiartsly <stiartsly@gmail.com>
2019-04-10 16:45:29 +0800

    Add test case to test login method

--- src/test/java/org/elastos/hive/HiveDriveTest.java
+++ src/test/java/org/elastos/hive/HiveDriveTest.java
@@ -2,6 +2,7 @@ package org.elastos.hive;
 
 import static org.junit.Assert.assertTrue;
 
+import org.elastos.hive.exceptions.HiveException;
 import org.junit.Test;
 
 public class HiveDriveTest {
@@ -16,9 +17,39 @@ public class HiveDriveTest {
 		}
 	}
 
-    @Test public void testGetRootDir() {
-    	//TODO;
-    	System.out.print("TODO");
-    	assertTrue("GetRootDir", true);
+	@Test public void testGetRootDir() {
+		//TODO;
+		System.out.print("TODO");
+		assertTrue("GetRootDir", true);
+	}
+
+	@Test public void testLogin() {
+		class TestAuthenticator extends Authenticator {
+			TestAuthenticator() {
+				//TODO;
+			}
+
+			@Override
+			public AuthResult requestAuthentication(String requestUrl) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		}
+
+		DriveParameters parameters = DriveParameters.createForOneDrive("3d0f9362-0f69-4a45-9ead-f653c2712290",
+				"offline_access Files.ReadWrite",
+				"https://login.microsoftonline.com/common/oauth2/nativeclient");
+
+		try {
+			HiveDrive drive = HiveDrive.createInstance(parameters);
+			assertTrue("Can't create drive instance.", drive != null);
+
+			TestAuthenticator testAuth = new TestAuthenticator();
+
+			drive.login(testAuth);
+			drive.logout();
+		} catch (HiveException e) {
+			e.printStackTrace();
+		}
     }
 }
