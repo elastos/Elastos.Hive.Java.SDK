f413998376e9c69b1a8dc644af61305aff6479dc
wanli <zhaowanli@elastos.org>
2019-05-31 18:35:40 +0800

    1. Add OneDriveFileTest; 2. debug OneDriveDirectory and OneDriveFile.

--- src/test/java/org/elastos/hive/OneDriveDirectoryTest.java
+++ src/test/java/org/elastos/hive/OneDriveDirectoryTest.java
@@ -1,6 +1,7 @@
 package org.elastos.hive;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
@@ -14,6 +15,7 @@ public class OneDriveDirectoryTest {
 	private static Drive drive;
 	private static Client client;
 	private static Directory testDirectory;
+	private static Directory parentDirForMoveTo;
 
 	@Test public void testRootDirectory() {
 		try {
@@ -139,17 +141,110 @@ public class OneDriveDirectoryTest {
 			final int count = children.getContent().size();
 
 			//Create several files and check the count.
-			final int newCount = 5;
-			for (int i = 0; i < newCount; i++) {
-				String childName = "testCreateFile" + System.currentTimeMillis();
+			final int NUM = 5;
+			for (int i = 0; i < NUM; i++) {
+				String childName = "testGetChildrenCount" + System.currentTimeMillis();
 				assertNotNull(testDirectory.createFile(childName).get());
 			}
 
 			children = testDirectory.getChildren().get();
-			assertEquals(count + newCount, children.getContent().size());
+			assertEquals(count + NUM, children.getContent().size());
 		} catch (InterruptedException | ExecutionException e) {
 			e.printStackTrace();
 			fail("testGetChildrenCount failed");
+		}
+	}
+
+	@Test public void testMoveTo() {
+		try {
+			String originPath = testDirectory.getPath();
+			testDirectory.moveTo(parentDirForMoveTo.getPath()).get();
+
+			//1. Check the parent has a new child.
+			Children children = parentDirForMoveTo.getChildren().get();
+			assertNotNull(children);
+			assertEquals(1, children.getContent().size());
+			
+			//2. Check: the origin and the new path is different
+			assertNotEquals(originPath, testDirectory.getPath());
+
+			//3. Check: the origin path is invalid.
+			try {
+				drive.getDirectory(originPath).get();
+				fail(String.format("The a directory has moved, the origin path is invalid: %s", originPath));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail("testMoveToInvalid failed");
+		}
+	}
+	
+	@Test public void testCopyTo() {
+		Directory parentDir = null;
+		try {
+			String parentPath = "/parentDir" + System.currentTimeMillis();
+			parentDir = drive.createDirectory(parentPath).get();
+			assertNotNull(parentDir);
+			Status status = testDirectory.copyTo(parentPath).get();
+			assertEquals(1, status.getStatus());
+
+			Thread.sleep(5000);
+			Children children = parentDir.getChildren().get();
+			assertNotNull(children);
+			assertEquals(1, children.getContent().size());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail("testCopyTo failed");
+		}
+		finally {
+			try {
+				parentDir.deleteItem().get();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				fail();
+			}
+		}
+	}
+
+	@Test public void testCopyToInvalid() {
+		Directory parentDir = null;
+		try {
+			String parentPath = "/parentDir" + System.currentTimeMillis();
+			parentDir = drive.createDirectory(parentPath).get();
+			assertNotNull(parentDir);
+			String originPath = testDirectory.getPath();
+			Status status = testDirectory.copyTo(parentPath).get();
+			assertEquals(1, status.getStatus());
+
+			//1. Check the parent has a new child.
+			Thread.sleep(2000);
+			Children children = parentDir.getChildren().get();
+			assertNotNull(children);
+			assertEquals(1, children.getContent().size());
+
+			//2. Check: the origin and the new path is same
+			assertEquals(originPath, testDirectory.getPath());
+
+			//3. Check: the origin path is valid.
+			try {
+				drive.getDirectory(originPath).get();
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail(String.format("The origin path should be valid: %s", originPath));
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail("testCopyToInvalid failed");
+		}
+		finally {
+			try {
+				parentDir.deleteItem().get();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				fail();
+			}
 		}
 	}
 
@@ -163,6 +258,8 @@ public class OneDriveDirectoryTest {
 			testDirectory = drive.createDirectory("/testDirectory" + System.currentTimeMillis()).get();
 			assertNotNull(testDirectory);
 			assertNotNull(testDirectory.getPath());
+			parentDirForMoveTo = drive.createDirectory("/parentDirForMoveTo" + System.currentTimeMillis()).get();
+			assertNotNull(parentDirForMoveTo);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -170,6 +267,18 @@ public class OneDriveDirectoryTest {
 
     @AfterClass
     static public void tearDown() throws Exception {
+    	try {
+    		testDirectory.deleteItem().get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+    	try {
+    		parentDirForMoveTo.deleteItem().get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
     	client.logout();
     }
 }
