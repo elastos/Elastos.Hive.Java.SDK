3bcad2e21f21b2f2d665f68b878f5bb92667f642
wanli <zhaowanli@elastos.org>
2019-06-05 10:47:35 +0800

    1. Add the files about Ipfs; 2. Add the testcase about IpfsClient.

---
+++ src/test/java/org/elastos/hive/IpfsClientTest.java
@@ -0,0 +1,48 @@
+package org.elastos.hive;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import java.util.concurrent.ExecutionException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IpfsClientTest {
+	private static Client client;
+
+	@Test public void testGetInstance() {
+		assertNotNull(Client.getInstance(DriveType.hiveIpfs));
+	}
+
+	@Test public void testGetInfo() {
+		try {
+			Client.Info info = client.getInfo().get();
+			assertNotNull(info);
+			assertNotNull(info.getUserId());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail("getInfo failed");
+		}
+	}
+
+	@Test public void testGetDefaultDrive() {
+		try {
+			Drive drive = client.getDefaultDrive().get();
+			assertNotNull(drive);
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail("getInfo failed");
+		}
+	}
+
+	@BeforeClass
+	static public void setUp() throws Exception {
+		client = IpfsTestBase.login();
+		assertNotNull(client);
+	}
+
+    @AfterClass
+    static public void tearDown() throws Exception {
+    	client.logout();
+    }
+}
