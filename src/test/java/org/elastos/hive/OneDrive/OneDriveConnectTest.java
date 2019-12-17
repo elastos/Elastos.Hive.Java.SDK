afa025ab5fb579765c87547fd01677617af42b60
snackwang <snackwang.petter@gmail.com>
2019-12-17 16:00:53 +0800

    Add storePath setting in onedriveConnectOption

--- src/test/java/org/elastos/hive/OneDrive/OneDriveConnectTest.java
+++ src/test/java/org/elastos/hive/OneDrive/OneDriveConnectTest.java
@@ -23,6 +23,7 @@ public class OneDriveConnectTest {
     private static final String APPID = "afd3d647-a8b7-4723-bf9d-1b832f43b881";//f0f8fdc1-294e-4d5c-b3d8-774147075480
     private static final String SCOPE = "User.Read Files.ReadWrite.All offline_access";//offline_access Files.ReadWrite
     private static final String REDIRECTURL = "http://localhost:12345";//http://localhost:44316
+    private static final String STORE_PATH = System.getProperty("user.dir");
 
     @Test
     public void testGetInstance() {
@@ -37,7 +38,7 @@ public class OneDriveConnectTest {
 
     @Test
     public void testConnect() {
-        HiveConnectOptions hiveConnectOptions = new OneDriveConnectOptions(APPID,SCOPE,REDIRECTURL, requestUrl -> {
+        HiveConnectOptions hiveConnectOptions = new OneDriveConnectOptions(APPID,SCOPE,REDIRECTURL, STORE_PATH,requestUrl -> {
             try {
                 Desktop.getDesktop().browse(new URI(requestUrl));
             }
