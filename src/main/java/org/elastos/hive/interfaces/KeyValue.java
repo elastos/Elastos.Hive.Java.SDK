0d4d321ffef9fa2a86097ae3e1217f14e6e34544
stiartsly <stiartsly@gmail.com>
2020-01-08 21:20:29 +0800

    Added interfaces

---
+++ src/main/java/org/elastos/hive/interfaces/KeyValue.java
@@ -0,0 +1,21 @@
+package org.elastos.hive.interfaces;
+
+import org.elastos.hive.Callback;
+import org.elastos.hive.result.ValueList;
+import org.elastos.hive.result.Void;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface KeyValue {
+    CompletableFuture<Void> putValue(String key, byte[] value);
+    CompletableFuture<Void> putValue(String key, byte[] value, Callback<Void> callback);
+
+    CompletableFuture<Void> setValue(String key, byte[] value);
+    CompletableFuture<Void> setValue(String key, byte[] value, Callback<Void> callback);
+
+    CompletableFuture<ValueList> getValues(String key);
+    CompletableFuture<ValueList> getValues(String key, Callback<ValueList> callback);
+
+    CompletableFuture<Void> delete(String key);
+    CompletableFuture<Void> delete(String key, Callback<Void> callback);
+}
