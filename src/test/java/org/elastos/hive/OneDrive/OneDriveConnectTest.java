18c54e0d178b002635f24578a99c4d92cf0fcec1
stiartsly <stiartsly@gmail.com>
2020-01-08 14:04:29 +0800

    typo fixes for OneDrive testsuite

--- src/test/java/org/elastos/hive/OneDrive/OneDriveConnectTest.java
+++ src/test/java/org/elastos/hive/OneDrive/OneDriveConnectTest.java
@@ -16,44 +16,46 @@ import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertNotNull;
 
 public class OneDriveConnectTest {
-    private static HiveConnect hiveConnect ;
-    private static Client hiveClient ;
+    private static Client client;
+    private static HiveConnect connect;
 
     private static final String APPID = "afd3d647-a8b7-4723-bf9d-1b832f43b881";//f0f8fdc1-294e-4d5c-b3d8-774147075480
-    private static final String SCOPE = "User.Read Files.ReadWrite.All offline_access";//offline_access Files.ReadWrite
+    // private static final String SCOPE = "User.Read Files.ReadWrite.All offline_access";//offline_access Files.ReadWrite
     private static final String REDIRECTURL = "http://localhost:12345";//http://localhost:44316
     private static final String STORE_PATH = System.getProperty("user.dir");
 
     @Test
     public void testGetInstance() {
-        assertNotNull(hiveConnect);
+        assertNotNull(connect);
     }
 
     @BeforeClass
     public static void setUp(){
-        ClientOptions hiveOptions = new ClientOptions.Builder().setStorePath(STORE_PATH).build();
-        hiveClient = new Client(hiveOptions);
+        ClientOptions options = new ClientOptions
+                .Builder()
+                .setStorePath(STORE_PATH)
+                .build();
+        client = new Client(options);
     }
 
     @Test
     public void testConnect() {
-        ConnectOptions hiveConnectOptions =
-                new OneDriveConnectOptions.Builder()
-                        .setClientId(APPID)
-                        .setRedirectUrl(REDIRECTURL)
-                        .setAuthenticator(requestUrl -> {
-                            try {
-                                Desktop.getDesktop().browse(new URI(requestUrl));
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                                fail("Authenticator failed");
-                            }
-                        }).build();
-        hiveConnect = hiveClient.connect(hiveConnectOptions);
+        ConnectOptions options = new OneDriveConnectOptions.Builder()
+                .setClientId(APPID)
+                .setRedirectUrl(REDIRECTURL)
+                .setAuthenticator(requestUrl -> {
+                    try {
+                        Desktop.getDesktop().browse(new URI(requestUrl));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        fail("Authenticator failed");
+                    }
+                }).build();
+        connect = client.connect(options);
     }
 
     @AfterClass
     public static void tearDown() {
-        hiveClient.disConnect(hiveConnect);
+        client.disConnect(connect);
     }
 }
