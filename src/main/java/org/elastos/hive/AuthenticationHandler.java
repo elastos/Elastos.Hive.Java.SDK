b9e3113458f25bbd509a8671101900bf1cdb1706
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-08-28 09:11:56 +0800

    Temporary fix to make the AuthenticationHandler async

--- src/main/java/org/elastos/hive/AuthenticationHandler.java
+++ src/main/java/org/elastos/hive/AuthenticationHandler.java
@@ -1,6 +1,13 @@
 package org.elastos.hive;
 
 public interface AuthenticationHandler {
+    public interface OnAuthenticationCompleted {
+        /**
+         * Callback to be called as a response to authenticationChallenge(), whenever the authentication
+         * response is ready to be sent to the vault (packaged and signed as JWT).
+         */
+        void onAuthenticationCompleted(String jwtToken);
+    }
     /*
      * auth challenge: JWT (iss, nonce)
      * hive sdk:
@@ -13,6 +20,6 @@ public interface AuthenticationHandler {
      *   verify jwt (using local app instance did public key provided before)
      *   generate access token
      */
-    public String authenticationChallenge(String jwtToken);
+    public String authenticationChallenge(String jwtToken, OnAuthenticationCompleted authCompletedCallback);
 }
 
