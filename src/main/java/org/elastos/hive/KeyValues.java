d9e221e6444e9c358f2ad1dabb894f67f98a5d70
stiartsly <stiartsly@gmail.com>
2020-10-21 20:57:32 +0800

    CU-epuggh: remove unnecessary override methods with parameter of Callback

--- src/main/java/org/elastos/hive/KeyValues.java
+++ src/main/java/org/elastos/hive/KeyValues.java
@@ -1,32 +1,18 @@
 package org.elastos.hive;
 
-import org.elastos.hive.exception.HiveException;
-
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
 
 public interface KeyValues {
-    CompletableFuture<Void> putValue(String key, String value);
+	CompletableFuture<Void> putValue(String key, String value);
 
-    CompletableFuture<Void> putValue(String key, String value, Callback<Void> callback);
+	CompletableFuture<Void> putValue(String key, byte[] value);
 
-    CompletableFuture<Void> putValue(String key, byte[] value);
+	CompletableFuture<Void> setValue(String key, String value);
 
-    CompletableFuture<Void> putValue(String key, byte[] value, Callback<Void> callback);
+	CompletableFuture<Void> setValue(String key, byte[] value);
 
-    CompletableFuture<Void> setValue(String key, String value);
+	CompletableFuture<ArrayList<byte[]>> getValues(String key);
 
-    CompletableFuture<Void> setValue(String key, String value, Callback<Void> callback);
-
-    CompletableFuture<Void> setValue(String key, byte[] value);
-
-    CompletableFuture<Void> setValue(String key, byte[] value, Callback<Void> callback);
-
-    CompletableFuture<ArrayList<byte[]>> getValues(String key);
-
-    CompletableFuture<ArrayList<byte[]>> getValues(String key, Callback<ArrayList<byte[]>> callback);
-
-    CompletableFuture<Void> deleteKey(String key);
-
-    CompletableFuture<Void> deleteKey(String key, Callback<Void> callback);
+	CompletableFuture<Void> deleteKey(String key);
 }
