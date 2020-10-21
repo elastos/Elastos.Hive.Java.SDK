033d1907815967d2189ec2fd2195e23e0eab084c
stiartsly <stiartsly@gmail.com>
2020-10-21 12:00:58 +0800

    CU-emu192: format improvement for Vault.java - use tab (size: 4) instead of whitespaces

--- src/main/java/org/elastos/hive/Vault.java
+++ src/main/java/org/elastos/hive/Vault.java
@@ -1,119 +1,117 @@
 package org.elastos.hive;
 
+import org.elastos.hive.vault.AuthHelper;
 import org.elastos.hive.vault.DatabaseClient;
 import org.elastos.hive.vault.FileClient;
 import org.elastos.hive.vault.ScriptClient;
-import org.elastos.hive.vault.AuthHelper;
 
 /**
  * Vault class
  *      Provide basic information of vault.
- *      Provide file, database, scripting, KeyValues instances
+ *      Provide interface instances of file, database, KeyValue and scripting.
  */
 public class Vault {
 
-    private Files files;
-    private Database database;
-    private Scripting scripting;
-    private KeyValues keyValues;
+	private Files files;
+	private Database database;
+	private Scripting scripting;
+	private KeyValues keyValues;
 
-    private String vaultProvider;
-    private String ownerDid;
-    private AuthHelper authHelper;
+	private String vaultProvider;
+	private String ownerDid;
+	private AuthHelper authHelper;
 
-    /**
-     * Vault construction method
-     *
-     * @param authHelper
-     *          sign in，authorize and cloud sync helper class instance
-     * @param vaultProvider
-     *          vault server provider address
-     * @param ownerDid
-     *          vault provider did
-     */
-    public Vault(AuthHelper authHelper, String vaultProvider, String ownerDid) {
+	/**
+	 * Vault constructor
+	 *
+	 * @param authHelper
+	 *          sign in，authorize and cloud sync helper class instance
+	 * @param vaultProvider
+	 *          vault server provider address
+	 * @param ownerDid
+	 *          vault provider did
+	 */
+	public Vault(AuthHelper authHelper, String vaultProvider, String ownerDid) {
+		this.authHelper = authHelper;
+		this.vaultProvider = vaultProvider;
+		this.ownerDid = ownerDid;
 
-        this.authHelper = authHelper;
-        this.files = new FileClient(authHelper);
-        this.database = new DatabaseClient(authHelper);
-        this.scripting = new ScriptClient(authHelper);
+		this.files = new FileClient(authHelper);
+		this.database = new DatabaseClient(authHelper);
+		this.scripting = new ScriptClient(authHelper);
 
-        this.vaultProvider = vaultProvider;
-        this.ownerDid = ownerDid;
+		authHelper.connect();
+	}
 
-        authHelper.connect();
-    }
+	/**
+	 * Get vault provider address
+	 * @return	the vault provider address.
+	 */
+	public String getProviderAddress() {
+		return this.vaultProvider;
+	}
 
-    /**
-     *  get vault provider address
-     * @return
-     */
-    public String getProviderAddress() {
-        return this.vaultProvider;
-    }
+	/**
+	 * Get vault owner did
+	 * @return	the vault owner DID.
+	 */
+	public String getOwnerDid() {
+		return this.ownerDid;
+	}
 
-    /**
-     * get vault owner did
-     * @return
-     */
-    public String getOwnerDid() {
-        return this.ownerDid;
-    }
+	/**
+	 * Get application id
+	 * @return 	the application id.
+	 */
+	public String getAppId() {
+		return this.authHelper.getAppId();
+	}
 
-    /**
-     * get application id
-     * @return
-     */
-    public String getAppId() {
-        return this.authHelper.getAppId();
-    }
+	/**
+	 * Get application did
+	 * @return	the application DID.
+	 */
+	public String getAppInstanceDid() {
+		return this.authHelper.getAppInstanceDid();
+	}
 
-    /**
-     * get application did
-     * @return
-     */
-    public String getAppInstanceDid() {
-        return this.authHelper.getAppInstanceDid();
-    }
+	/**
+	 * Get user did
+	 * @return	the user DID.
+	 */
+	public String getUserDid() {
+		return this.authHelper.getUserDid();
+	}
 
-    /**
-     * get user did
-     * @return
-     */
-    public String getUserDid() {
-        return this.authHelper.getUserDid();
-    }
+	/**
+	 * Get the interface as database instance
+	 * @return interface instance of Database.
+	 */
+	public Database getDatabase() {
+		return this.database;
+	}
 
-    /**
-     * get vault's Database instance
-     * @return
-     */
-    public Database getDatabase() {
-        return this.database;
-    }
+	/**
+	 * Get the interface as Files instance
+	 * @return interface instance of Files.
+	 */
+	public Files getFiles() {
+		return this.files;
+	}
 
-    /**
-     * get vault's Files instance
-     * @return
-     */
-    public Files getFiles() {
-        return this.files;
-    }
+	/**
+	 * Get interface as KeyValues instance
+	 * @return interface instance of KeyValues
+	 */
+	public KeyValues getKeyValues() {
+		return this.keyValues;
+	}
 
-    /**
-     * get vault's KeyValues instance
-     * @return
-     */
-    public KeyValues getKeyValues() {
-        return this.keyValues;
-    }
-
-    /**
-     * get vault's Scripting instance
-     * @return
-     */
-    public Scripting getScripting() {
-        return this.scripting;
-    }
+	/**
+	 * Get interface as Scripting instance
+	 * @return	interface instance of Scripting
+	 */
+	public Scripting getScripting() {
+		return this.scripting;
+	}
 }
-
