5ee270a7b88a12da066d73c263975388a2a683f3
stiartsly <stiartsly@gmail.com>
2020-10-31 17:37:52 +0800

    CU-fcvfy7 - Formatting FileInfo.java with java-code style

--- src/main/java/org/elastos/hive/file/FileInfo.java
+++ src/main/java/org/elastos/hive/file/FileInfo.java
@@ -1,54 +1,48 @@
 package org.elastos.hive.file;
 
-import org.elastos.hive.utils.DateUtil;
-
 import java.math.BigDecimal;
 
+import org.elastos.hive.utils.DateUtil;
+
 /**
- * File information about a remote file or folder.
+ * The class to represent the information of File or Folder.
  */
 public class FileInfo {
+	/**
+	 * Type of a remote file or folder.
+	 */
+	public enum Type {
+		/** File */
+		FILE,
+		/** Folder */
+		FOLDER
+	};
 
-    /**
+	private String type;
+	private String name;
+	private long size;
+	private String lastModified;
 
-     * Type of a remote file or folder.
-     */
-    public enum Type {
-        /** File */
-        FILE,
-        /** Folder */
-        FOLDER
-    };
+	public Type getType() {
+		switch (type) {
+		case "file":
+			return Type.FILE;
+		case "folder":
+			return Type.FOLDER;
+		}
+		return null;
+	}
 
-    private String type;
+	public String getName() {
+		return name;
+	}
 
-    private String name;
+	public long getSize() {
+		return size;
+	}
 
-    private long size;
-
-    private String last_modify;
-
-
-    public Type getType() {
-        switch (type) {
-            case "file":
-                return Type.FILE;
-            case "folder":
-                return Type.FOLDER;
-        }
-        return null;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public String getLastModify() {
-        long timeStamp = new BigDecimal(last_modify).multiply(new BigDecimal(1000)).longValue();
-        return DateUtil.getCurrentEpochTimeStamp(timeStamp);
-    }
+	public String getLastModified() {
+		long timeStamp = new BigDecimal(lastModified).multiply(new BigDecimal(1000)).longValue();
+		return DateUtil.getCurrentEpochTimeStamp(timeStamp);
+	}
 }
