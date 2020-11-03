0e18542751901dc4463b969a3b440a867e9392dc
xidaokun <xidaokun@elastos.org>
2020-11-03 14:26:39 +0800

    CU-4zvgep add file download executable

---
+++ src/main/java/org/elastos/hive/scripting/FileDownload.java
@@ -0,0 +1,15 @@
+package org.elastos.hive.scripting;
+
+
+public class FileDownload extends FileExecutable {
+	private static final String TYPE = "fileDownload";
+
+	public FileDownload(String name, String path) {
+		super(TYPE, name, path);
+	}
+
+	public FileDownload(String name, String path, boolean output) {
+		super(TYPE, name, path, output);
+	}
+
+}
