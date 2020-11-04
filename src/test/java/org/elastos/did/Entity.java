94011134b667a4abce7025371009f3768bf94e3c
xidaokun <xidaokun@elastos.org>
2020-11-04 09:32:20 +0800

    CU-4zvnht refactor presentation jwt

---
+++ src/test/java/org/elastos/did/Entity.java
@@ -0,0 +1,79 @@
+package org.elastos.did;
+
+import org.elastos.did.adapter.DummyAdapter;
+import org.elastos.did.exception.DIDException;
+
+import java.io.File;
+import java.util.List;
+
+public class Entity {
+	private String phrasepass;
+	protected String storepass;
+
+	private String name;
+	private DIDStore store;
+	private DID did;
+
+	private static DummyAdapter adapter;
+
+	protected Entity(String name, String mnemonic, DummyAdapter adapter, String phrasepass, String storepass) throws DIDException {
+		this.phrasepass = phrasepass;
+		this.storepass = storepass;
+		this.name = name;
+		this.adapter = adapter;
+
+		initPrivateIdentity(mnemonic);
+		initDid();
+	}
+
+	protected void initPrivateIdentity(String mnemonic) throws DIDException {
+		final String storePath = System.getProperty("user.dir") + File.separator + "store" + File.separator + name;
+
+		store = DIDStore.open("filesystem", storePath, adapter);
+
+		if (store.containsPrivateIdentity())
+			return; // Already exists
+
+		store.initPrivateIdentity(null, mnemonic, phrasepass, storepass);
+	}
+
+	protected void initDid() throws DIDException {
+		List<DID> dids = store.listDids(DIDStore.DID_HAS_PRIVATEKEY);
+		if (dids.size() > 0) {
+			for (DID did : dids) {
+				if (did.getMetadata().getAlias().equals("me")) {
+					System.out.format("[%s] My DID: %s%n", name, did);
+					this.did = did;
+
+					store.publishDid(did, storepass);
+					return;
+				}
+			}
+		}
+
+		DIDDocument doc = store.newDid("me", storepass);
+		this.did = doc.getSubject();
+		System.out.format("[%s] My new DID created: %s%n", name, did);
+		store.publishDid(did, storepass);
+	}
+
+	protected DIDStore getDIDStore() {
+		return store;
+	}
+
+	public DID getDid() {
+		return did;
+	}
+
+	public DIDDocument getDocument() throws DIDException {
+		return store.loadDid(did);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	protected String getStorePassword() {
+		return storepass;
+	}
+}
--- src/test/java/org/elastos/did/PresentationInJWT.java
+++ src/test/java/org/elastos/did/PresentationInJWT.java
@@ -22,225 +22,66 @@
 
 package org.elastos.did;
 
-import org.elastos.did.Issuer.CredentialBuilder;
 import org.elastos.did.adapter.DummyAdapter;
-import org.elastos.did.exception.DIDException;
 import org.elastos.did.jwt.Claims;
-import org.elastos.did.jwt.Header;
 import org.elastos.hive.utils.JwtUtil;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class PresentationInJWT {
-    private static DummyAdapter adapter;
-    public static class Entity {
-        private final static String passphrase = "password";
-        protected final static String storepass = "password";
-
-        private String name;
-        private DIDStore store;
-        private DID did;
-
-        protected Entity(String name, String mnemonic) throws DIDException {
-            this.name = name;
-
-            initPrivateIdentity(mnemonic);
-            initDid();
-        }
-
-        protected void initPrivateIdentity(String mnemonic) throws DIDException {
-            final String storePath = System.getProperty("user.dir") + File.separator + "store" + File.separator + name;
-
-            store = DIDStore.open("filesystem", storePath, adapter);
-
-            if (store.containsPrivateIdentity())
-                return; // Already exists
-
-            store.initPrivateIdentity(null, mnemonic, passphrase, storepass);
-        }
-
-        protected void initDid() throws DIDException {
-            List<DID> dids = store.listDids(DIDStore.DID_HAS_PRIVATEKEY);
-            if (dids.size() > 0) {
-                for (DID did : dids) {
-                    if (did.getMetadata().getAlias().equals("me")) {
-                        System.out.format("[%s] My DID: %s%n", name, did);
-                        this.did = did;
-
-                        store.publishDid(did, storepass);
-                        return;
-                    }
-                }
-            }
-
-            DIDDocument doc = store.newDid("me", storepass);
-            this.did = doc.getSubject();
-            System.out.format("[%s] My new DID created: %s%n", name, did);
-            store.publishDid(did, storepass);
-        }
-
-        protected DIDStore getDIDStore() {
-            return store;
-        }
-
-        public DID getDid() {
-            return did;
-        }
-
-        public DIDDocument getDocument() throws DIDException {
-            return store.loadDid(did);
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        protected String getStorePassword() {
-            return storepass;
-        }
-    }
-
-    public static class DIDApp extends Entity {
-        private Issuer issuer;
-
-        public DIDApp(String name, String mnemonic) throws DIDException {
-            super(name, mnemonic);
-
-            issuer = new Issuer(getDocument());
-        }
-
-        public VerifiableCredential issueDiplomaFor(DApp dapp) throws DIDException {
-            Map<String, String> subject = new HashMap<String, String>();
-            subject.put("appDid", dapp.appId);
-
-            Calendar exp = Calendar.getInstance();
-            exp.add(Calendar.YEAR, 5);
-
-            CredentialBuilder cb = issuer.issueFor(dapp.getDid());
-            VerifiableCredential vc = cb.id("didapp")
-                    .type("AppIdCredential")
-                    .properties(subject)
-                    .expirationDate(exp.getTime())
-                    .seal(getStorePassword());
-
-            System.out.println("VerifiableCredential:");
-            String vcStr = vc.toJson(true, true);
-            System.out.println(vcStr);
-
-            return vc;
-        }
-    }
-
-    public static class DApp extends Entity {
-        public String appId = "appId";
-
-        public DApp(String name, String mnemonic) throws DIDException {
-            super(name, mnemonic);
-        }
-
-        public VerifiablePresentation createPresentation(VerifiableCredential vc, String realm, String nonce) throws DIDException {
-            VerifiablePresentation.Builder vpb = VerifiablePresentation.createFor(getDid(), getDIDStore());
-            List<VerifiableCredential> vcs = new ArrayList<VerifiableCredential>(1);
-            vcs.add(vc);
-
-            VerifiablePresentation vp = vpb.credentials(vcs.toArray(new VerifiableCredential[vcs.size()]))
-                    .realm(realm)
-                    .nonce(nonce)
-                    .seal(getStorePassword());
-
-            System.out.println("VerifiableCredential:");
-            String vpStr = vp.toJson(true);
-            System.out.println(vpStr);
-
-            return vp;
-        }
-
-        public String createToken(VerifiablePresentation vp, String hiveDid) throws DIDException {
-
-            Calendar cal = Calendar.getInstance();
-            cal.set(Calendar.MILLISECOND, 0);
-            Date iat = cal.getTime();
-            Date nbf = cal.getTime();
-            cal.add(Calendar.MONTH, 3);
-            Date exp = cal.getTime();
-
-            // Create JWT token with presentation.
-            String token = getDocument().jwtBuilder()
-                    .addHeader(Header.TYPE, Header.JWT_TYPE)
-                    .addHeader("version", "1.0")
-
-                    .setSubject("DIDAuthResponse")
-                    .setAudience(hiveDid)
-                    .setIssuedAt(iat)
-                    .setExpiration(exp)
-                    .setNotBefore(nbf)
-                    .claimWithJson("presentation", vp.toString())
-                    .sign(storepass)
-                    .compact();
-
-            System.out.println("JWT Token:");
-            System.out.println("  " + token);
-            return token;
-        }
-
-    }
-
-    static void initDIDBackend() {
-        final String cacheDir = System.getProperty("user.dir") + File.separator + "store" + File.separator + "cache";
-
-        adapter = new DummyAdapter();
-        DIDBackend.initialize(adapter, cacheDir);
-    }
-
     DIDApp didapp = null;
     DApp testapp = null;
     String docStr = null;
     DIDDocument doc = null;
-    public PresentationInJWT init() {
-        try {
-            initDIDBackend();
-            didapp = new DIDApp("didapp", "provide zero slab drink patient tape private paddle unaware catch virtual stone");
-            testapp = new DApp("testapp", "polar degree weapon crouch alarm scorpion between stand glow round catalog marine");
 
-            doc = testapp.getDocument();
-            docStr = doc.toJson(true, true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	private static DummyAdapter adapter;
 
-        return this;
-    }
+	private void initDIDBackend() {
+		final String cacheDir = System.getProperty("user.dir") + File.separator + "store" + File.separator + "cache";
 
-    public String getDocStr() {
-        return docStr;
-    }
+		adapter = new DummyAdapter();
+		DIDBackend.initialize(adapter, cacheDir);
+	}
 
-    public DIDDocument getDoc() {
-        return doc;
-    }
+	public PresentationInJWT init() {
+		try {
+			initDIDBackend();
+			didapp = new DIDApp("didapp", "provide zero slab drink patient tape private paddle unaware catch virtual stone", adapter, "password", "password");
+			testapp = new DApp("testapp", "polar degree weapon crouch alarm scorpion between stand glow round catalog marine", adapter, "password", "password");
 
-    public String getAuthToken(String jwtToken) {
-        try {
-            Claims claims = JwtUtil.getBody(jwtToken);
-            String iss = claims.getIssuer();
-            String nonce = (String) claims.get("nonce");
+			doc = testapp.getDocument();
+			docStr = doc.toJson(true, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            VerifiableCredential vc = didapp.issueDiplomaFor(testapp);
+		return this;
+	}
 
-            VerifiablePresentation vp = testapp.createPresentation(vc, iss, nonce);
+	public String getDocStr() {
+		return docStr;
+	}
 
-            String token = testapp.createToken(vp, iss);
-            return token;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public DIDDocument getDoc() {
+		return doc;
+	}
+
+	public String getAuthToken(String jwtToken) {
+		try {
+			Claims claims = JwtUtil.getBody(jwtToken);
+			String iss = claims.getIssuer();
+			String nonce = (String) claims.get("nonce");
+
+			VerifiableCredential vc = didapp.issueDiplomaFor(testapp);
+
+			VerifiablePresentation vp = testapp.createPresentation(vc, iss, nonce);
+
+			String token = testapp.createToken(vp, iss);
+			return token;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
