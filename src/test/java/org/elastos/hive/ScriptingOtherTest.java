e214d746b827a98874d0ab953513b95c5fc6cc35
xidaokun <xidaokun@elastos.org>
2020-11-04 23:28:09 +0800

    CU-4zvnht cross did test case

---
+++ src/test/java/org/elastos/hive/ScriptingOtherTest.java
@@ -0,0 +1,29 @@
+package org.elastos.hive;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ScriptingOtherTest {
+
+	private String noConditionName = "get_groups";
+
+	@Test
+	public void test00_callOtherScript() {
+		try {
+			String ret = scripting.call(noConditionName, String.class).get();
+			System.out.println("return=" + ret);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+
+	private static Scripting scripting;
+
+	@BeforeClass
+	public static void setUp() {
+		Vault vault = UserFactory.createUser2().getVault();
+		scripting = vault.getScripting();
+	}
+}
