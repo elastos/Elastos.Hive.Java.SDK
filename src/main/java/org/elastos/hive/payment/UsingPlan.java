76281c64f7ac6bde7126d597f1234484a05c0aff
xidaokun <xidaokun@elastos.org>
2020-11-11 14:43:29 +0800

    CU-fwvp4e test getUsingPlan api

--- src/main/java/org/elastos/hive/payment/UsingPlan.java
+++ src/main/java/org/elastos/hive/payment/UsingPlan.java
@@ -6,55 +6,50 @@ import org.elastos.hive.database.Result;
 
 public class UsingPlan extends Result<UsingPlan> {
 
-	@JsonProperty("vault_service_info")
-	private UsingInfo usingPlan;
+	@JsonProperty("max_storage")
+	private int maxStorage;
+	@JsonProperty("file_use_storage")
+	private int fileUseStorage;
+	@JsonProperty("db_use_storage")
+	private int dbUseStorage;
+	@JsonProperty("modify_time")
+	private long modifyTime;
+	@JsonProperty("start_time")
+	private long startTime;
+	@JsonProperty("end_time")
+	private long endTime;
+	@JsonProperty("pricing_using")
+	private String pricingUsing;
 
-	public static class UsingInfo extends Result<UsingInfo> {
-		@JsonProperty("max_storage")
-		private int maxStorage;
-		@JsonProperty("file_use_storage")
-		private int fileUseStorage;
-		@JsonProperty("db_use_storage")
-		private int dbUseStorage;
-		@JsonProperty("modify_time")
-		private long modifyTime;
-		@JsonProperty("start_time")
-		private long startTime;
-		@JsonProperty("end_time")
-		private long endTime;
-		@JsonProperty("pricing_using")
-		private String pricingUsing;
+	public int maxStorage() {
+		return maxStorage;
+	}
 
-		public int maxStorage() {
-			return maxStorage;
-		}
+	public int fileUseStorage() {
+		return fileUseStorage;
+	}
 
-		public int fileUseStorage() {
-			return fileUseStorage;
-		}
+	public int dbUseStorage() {
+		return dbUseStorage;
+	}
 
-		public int dbUseStorage() {
-			return dbUseStorage;
-		}
+	public long modifyTime() {
+		return modifyTime;
+	}
 
-		public long modifyTime() {
-			return modifyTime;
-		}
+	public long startTime() {
+		return startTime;
+	}
 
-		public long startTime() {
-			return startTime;
-		}
+	public long endTime() {
+		return endTime;
+	}
 
-		public long endTime() {
-			return endTime;
-		}
+	public String pricingUsing() {
+		return pricingUsing;
+	}
 
-		public String pricingUsing() {
-			return pricingUsing;
-		}
-
-		public static UsingInfo deserialize(String content) {
-			return deserialize(content, UsingInfo.class);
-		}
+	public static UsingPlan deserialize(String content) {
+		return deserialize(content, UsingPlan.class);
 	}
 }
