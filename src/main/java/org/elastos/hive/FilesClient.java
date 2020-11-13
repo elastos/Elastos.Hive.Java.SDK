c862910bf286c4486d0f1f1a83f01e3427bcaf6e
stiartsly <stiartsly@gmail.com>
2020-11-13 18:21:53 +0800

    CU-g0vu53 refine internal implementation structure of FilesClient.java

--- src/main/java/org/elastos/hive/FilesClient.java
+++ src/main/java/org/elastos/hive/FilesClient.java
@@ -38,230 +38,242 @@ class FilesClient implements Files {
 
 	@Override
 	public <T> CompletableFuture<T> upload(String path, Class<T> resultType) {
-		return authHelper.checkValid()
-				.thenCompose(result -> uploadImp(path, resultType));
-	}
-
-	private <T> CompletableFuture<T> uploadImp(String path, Class<T> resultType) {
-		return CompletableFuture.supplyAsync(() -> {
-			HttpURLConnection connection = null;
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				connection = this.connectionManager.openURLConnection(path);
-				OutputStream outputStream = connection.getOutputStream();
-
-				if (outputStream == null) {
-					HiveException e = new HiveException("Connection failure");
-					throw new CompletionException(e);
-				}
-
-				if(resultType.isAssignableFrom(OutputStream.class)) {
-					UploadOutputStream uploader = new UploadOutputStream(connection, outputStream);
-					return resultType.cast(uploader);
-				}
-
-				if (resultType.isAssignableFrom(OutputStreamWriter.class)) {
-					OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-					return resultType.cast(writer);
-				}
-
-				HiveException e = new HiveException("Not supported result type");
+				return uploadImpl(path, resultType);
+			} catch (HiveException e) {
+				e.printStackTrace();
 				throw new CompletionException(e);
-
-			} catch (Exception e) {
-				ResponseHelper.readConnection(connection);
-				HiveException ex = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(ex);
 			}
 		});
+	}
+
+	private <T> T uploadImpl(String path, Class<T> resultType) throws HiveException {
+		HttpURLConnection connection = null;
+		try {
+			connection = this.connectionManager.openURLConnection(path);
+			OutputStream outputStream = connection.getOutputStream();
+
+			if (outputStream == null)
+				throw new HiveException("Connection failure");
+
+			if(resultType.isAssignableFrom(OutputStream.class)) {
+				UploadOutputStream uploader = new UploadOutputStream(connection, outputStream);
+				return resultType.cast(uploader);
+			}
+			if (resultType.isAssignableFrom(OutputStreamWriter.class)) {
+				OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+				return resultType.cast(writer);
+			}
+
+			throw new HiveException("Not supported result type");
+		} catch (Exception e) {
+			throw new HiveException(e.getMessage());
+		}
 	}
 
 	@Override
 	public <T> CompletableFuture<T> download(String path, Class<T> resultType) {
-		return authHelper.checkValid()
-				.thenCompose(result -> downloadImp(path, resultType));
-	}
-
-	private <T> CompletableFuture<T> downloadImp(String remoteFile, Class<T> resultType) {
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.downloader(remoteFile)
-						.execute();
-				if (response == null)
-					throw new HiveException(HiveException.ERROR);
-
-				authHelper.checkResponseWithRetry(response);
-
-				if(resultType.isAssignableFrom(Reader.class)) {
-					Reader reader = ResponseHelper.getToReader(response);
-					return resultType.cast(reader);
-				}
-				if (resultType.isAssignableFrom(InputStream.class)){
-					InputStream inputStream = ResponseHelper.getInputStream(response);
-					return resultType.cast(inputStream);
-				}
-
-				HiveException e = new HiveException("Not supported result type");
+				return downloadImpl(path, resultType);
+			} catch (HiveException e) {
+				e.printStackTrace();
 				throw new CompletionException(e);
-
-			} catch (Exception e) {
-				HiveException ex = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(ex);
 			}
 		});
+	}
+
+	private <T> T downloadImpl(String remoteFile, Class<T> resultType) throws HiveException {
+		try {
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.downloader(remoteFile)
+					.execute();
+			if (response == null)
+				throw new HiveException(HiveException.ERROR);
+
+			authHelper.checkResponseWithRetry(response);
+
+			if(resultType.isAssignableFrom(Reader.class)) {
+				Reader reader = ResponseHelper.getToReader(response);
+				return resultType.cast(reader);
+			}
+			if (resultType.isAssignableFrom(InputStream.class)){
+				InputStream inputStream = ResponseHelper.getInputStream(response);
+				return resultType.cast(inputStream);
+			}
+
+			throw new HiveException("Not supported result type");
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public CompletableFuture<Boolean> delete(String remoteFile) {
-		return authHelper.checkValid()
-				.thenCompose(result -> deleteImp(remoteFile));
-	}
-
-	private CompletableFuture<Boolean> deleteImp(String remoteFile) {
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-				map.put("path", remoteFile);
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.deleteFolder(createJsonRequestBody(json))
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				return true;
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return deleteImpl(remoteFile);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private Boolean deleteImpl(String remoteFile) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("path", remoteFile);
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.deleteFolder(createJsonRequestBody(json))
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			return true;
+		} catch (Exception e) {
+			throw new HiveException(e.getMessage());
+		}
 	}
 
 	@Override
-	public CompletableFuture<Boolean> move(String src, String dst) {
-		return authHelper.checkValid()
-				.thenCompose(result -> moveImp(src, dst));
-	}
-
-	private CompletableFuture<Boolean> moveImp(String src, String dst) {
-
-		return CompletableFuture.supplyAsync(() -> {
+	public CompletableFuture<Boolean> move(String source, String dest) {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-				map.put("src_path", src);
-				map.put("dst_path", dst);
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.move(createJsonRequestBody(json))
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				return true;
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return moveImpl(source, dest);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private Boolean moveImpl(String source, String dest) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("src_path", source);
+			map.put("dst_path", dest);
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.move(createJsonRequestBody(json))
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			return true;
+		} catch (Exception e) {
+			throw new HiveException(e.getMessage());
+		}
 	}
 
 	@Override
-	public CompletableFuture<Boolean> copy(String src, String dst) {
-		return authHelper.checkValid()
-				.thenCompose(result -> copyImp(src, dst));
-	}
-
-	private CompletableFuture<Boolean> copyImp(String src, String dst) {
-
-		return CompletableFuture.supplyAsync(() -> {
+	public CompletableFuture<Boolean> copy(String source, String dest) {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-				map.put("src_path", src);
-				map.put("dst_path", dst);
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.copy(createJsonRequestBody(json))
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				return true;
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return copyImpl(source, dest);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private Boolean copyImpl(String source, String dest) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("src_path", source);
+			map.put("dst_path", dest);
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.copy(createJsonRequestBody(json))
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			return true;
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public CompletableFuture<String> hash(String remoteFile) {
-		return authHelper.checkValid()
-				.thenCompose(result -> hashImp(remoteFile));
-	}
-
-	private CompletableFuture<String> hashImp(String remoteFile) {
-
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Response response = this.connectionManager.getVaultApi()
-						.hash(remoteFile)
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				JsonNode ret = ResponseHelper.getValue(response, JsonNode.class);
-				return ret.get("SHA256").toString();
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return hashImp(remoteFile);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private String hashImp(String remoteFile) throws HiveException {
+		try {
+			Response response = this.connectionManager.getVaultApi()
+					.hash(remoteFile)
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			JsonNode ret = ResponseHelper.getValue(response, JsonNode.class);
+			return ret.get("SHA256").toString();
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public CompletableFuture<List<FileInfo>> list(String folder) {
-		return authHelper.checkValid()
-				.thenCompose(result -> listImp(folder));
-	}
-
-	private CompletableFuture<List<FileInfo>> listImp(String folder) {
-
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				NodeApi api = this.connectionManager.getVaultApi();
-				Response<FilesList> response = api.files(folder).execute();
-
-				authHelper.checkResponseWithRetry(response);
-				return response.body().getFiles();
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return listImpl(folder);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private List<FileInfo> listImpl(String folder) throws HiveException {
+		try {
+			NodeApi api = this.connectionManager.getVaultApi();
+			Response<FilesList> response = api.files(folder).execute();
+
+			authHelper.checkResponseWithRetry(response);
+			return response.body().getFiles();
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public CompletableFuture<FileInfo> stat(String path) {
-		return authHelper.checkValid()
-				.thenCompose(result -> statImp(path));
-	}
-
-	public CompletableFuture<FileInfo> statImp(String path) {
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				NodeApi api = this.connectionManager.getVaultApi();
-				Response<FileInfo> response = api.getProperties(path).execute();
-
-				authHelper.checkResponseWithRetry(response);
-				return response.body();
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
+				return statImpl(path);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
+	}
+
+	private FileInfo statImpl(String path) throws HiveException {
+		try {
+			NodeApi api = this.connectionManager.getVaultApi();
+			Response<FileInfo> response = api.getProperties(path).execute();
+
+			authHelper.checkResponseWithRetry(response);
+			return response.body();
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 
 	private RequestBody createJsonRequestBody(String json) {
