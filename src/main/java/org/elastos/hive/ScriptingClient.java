bad1ad6470be3296d44c1906c196ce5ad49f820c
stiartsly <stiartsly@gmail.com>
2020-11-14 15:13:28 +0800

    CU-g0vu53: refinement for scriptingClient.java

--- src/main/java/org/elastos/hive/ScriptingClient.java
+++ src/main/java/org/elastos/hive/ScriptingClient.java
@@ -1,6 +1,7 @@
 package org.elastos.hive;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.HashMap;
@@ -41,45 +42,46 @@ class ScriptingClient implements Scripting {
 
 	@Override
 	public CompletableFuture<Boolean> registerScript(String name, Condition condition, Executable executable) {
-		return authHelper.checkValid()
-				.thenCompose(result -> registerScriptImp(name, condition, executable));
-	}
-
-	private CompletableFuture<Boolean> registerScriptImp(String name, Condition condition, Executable executable) {
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-
-				map.put("name", name);
-				map.put("executable", executable);
-				if (condition != null)
-					map.put("condition", condition);
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.registerScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
-						.execute();
-
-				authHelper.checkResponseWithRetry(response);
-				return true;
-			} catch (Exception e) {
-				throw new CompletionException(new HiveException(e.getMessage()));
+				return registerScriptImpl(name, condition, executable);
+			} catch (HiveException e) {
+				e.printStackTrace();
+				throw new CompletionException(e);
 			}
 		});
 	}
 
-	@Override
-	public <T> CompletableFuture<T> call(String scriptName, Class<T> clazz) {
-		return authHelper.checkValid()
-				.thenCompose(result -> callImp(scriptName, null,null, clazz));
+	private boolean registerScriptImpl(String name, Condition condition, Executable executable) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("name", name);
+			map.put("executable", executable);
+			if (condition != null)
+				map.put("condition", condition);
+
+			String json = JsonUtil.serialize(map);
+
+			Response<ResponseBody> response;
+			response = this.connectionManager.getVaultApi()
+					.registerScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
+					.execute();
+
+			authHelper.checkResponseWithRetry(response);
+			return true;
+		} catch (IOException e) {
+			throw new HiveException(new HiveException(e.getMessage()));
+		}
 	}
 
 	@Override
-	public <T> CompletableFuture<T> call(String scriptName, JsonNode params, Class<T> clazz) {
-		return authHelper.checkValid()
-				.thenCompose(result -> callImp(scriptName, params, null, clazz));
+	public <T> CompletableFuture<T> call(String scriptName, Class<T> resultType) {
+		return this.call(scriptName, null, (String)null, resultType);
+	}
+
+	@Override
+	public <T> CompletableFuture<T> call(String scriptName, JsonNode params, Class<T> resultType) {
+		return this.call(scriptName, params, (String)null, resultType);
 	}
 
 	@Override
@@ -89,118 +91,118 @@ class ScriptingClient implements Scripting {
 
 	@Override
 	public <T> CompletableFuture<T> call(String scriptName, JsonNode params, String appDid, Class<T> resultType) {
-		return authHelper.checkValid()
-				.thenCompose(result -> callImp(scriptName, params, appDid, resultType));
-	}
-
-	private <T> CompletableFuture<T> callImp(String scriptName, JsonNode params, String appDid, Class<T> clazz) {
-		return CompletableFuture.supplyAsync(() -> {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-				map.put("name", scriptName);
-				if (params != null)
-					map.put("params", params);
-
-				ObjectNode targetNode = JsonNodeFactory.instance.objectNode();
-				String ownerDid = this.authHelper.getOwnerDid();
-				if(null != ownerDid) {
-					targetNode.put("target_did", ownerDid);
-					if (null != appDid)
-						targetNode.put("target_app_did", appDid);
-					map.put("context", targetNode);
-				}
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.callScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				return ResponseHelper.getValue(response, clazz);
-			} catch (Exception e) {
-				HiveException exception = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(exception);
-			}
-		});
-	}
-
-	@Override
-	public <T> CompletableFuture<T> call(String name, JsonNode params, Type type, Class<T> resultType) {
-		return authHelper.checkValid()
-				.thenCompose(result -> {
-					if (type == Type.UPLOAD) {
-						return fileUploadImp(name, params, resultType);
-					} else if (type == Type.DOWNLOAD) {
-						return fileDownloadImp(name, params, resultType);
-					} else if (type == Type.PROPERTIES) {
-						return callImp(name, params, null, resultType);
-					}
-					return null;
-				});
-	}
-
-	private <T> CompletableFuture<T> fileUploadImp(String filePath, JsonNode params, Class<T> resultType) {
-		return CompletableFuture.supplyAsync(() -> {
-			try {
-				String json = params.toString();
-				File file = new File(filePath);
-				RequestBody requestFile =
-						RequestBody.create(MediaType.parse("multipart/form-data"), file);
-				MultipartBody.Part body = MultipartBody.Part.createFormData("data", file.getName(), requestFile);
-
-				RequestBody metadata =
-						RequestBody.create(
-								MediaType.parse("application/json"), json);
-
-				Response<ResponseBody> response = this.connectionManager.getVaultApi()
-						.callScript(body, metadata)
-						.execute();
-				authHelper.checkResponseWithRetry(response);
-				return resultType.cast(ResponseHelper.toString(response));
-			} catch (Exception e) {
+				return callScriptImpl(scriptName, params, appDid, resultType);
+			} catch (HiveException e) {
 				e.printStackTrace();
 				throw new CompletionException(e);
 			}
 		});
 	}
 
-	private <T> CompletableFuture<T> fileDownloadImp(String scriptName, JsonNode params, Class<T> resultType) {
+	private <T> T callScriptImpl(String scriptName, JsonNode params, String appDid, Class<T> clazz) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("name", scriptName);
+			if (params != null)
+				map.put("params", params);
 
-		return CompletableFuture.supplyAsync(() -> {
+			ObjectNode targetNode = JsonNodeFactory.instance.objectNode();
+			String ownerDid = this.authHelper.getOwnerDid();
+			if(null != ownerDid) {
+				targetNode.put("target_did", ownerDid);
+				if (null != appDid)
+					targetNode.put("target_app_did", appDid);
+				map.put("context", targetNode);
+			}
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.callScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			return ResponseHelper.getValue(response, clazz);
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
+	}
+
+	@Override
+	public <T> CompletableFuture<T> call(String name, JsonNode params, Type type, Class<T> resultType) {
+		return authHelper.checkValid().thenApply(aVoid -> {
 			try {
-				Map<String, Object> map = new HashMap<>();
-
-				map.put("name", scriptName);
-				if (params != null)
-					map.put("params", params);
-
-				String json = JsonUtil.serialize(map);
-				Response<ResponseBody> response;
-
-				response = this.connectionManager.getVaultApi()
-						.callScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
-						.execute();
-
-				if (response == null)
-					throw new HiveException(HiveException.ERROR);
-
-				authHelper.checkResponseWithRetry(response);
-				if (resultType.isAssignableFrom(Reader.class)) {
-					Reader reader = ResponseHelper.getToReader(response);
-					return resultType.cast(reader);
+				if (type == Type.UPLOAD) {
+					return uploadFileImpl(name, params, resultType);
+				} else if (type == Type.DOWNLOAD) {
+					return downloadFileImpl(name, params, resultType);
+				} else if (type == Type.PROPERTIES) {
+					return callScriptImpl(name, params, (String)null, resultType);
 				}
-				if (resultType.isAssignableFrom(InputStream.class)) {
-					InputStream inputStream = ResponseHelper.getInputStream(response);
-					return resultType.cast(inputStream);
-				}
-
-				HiveException e = new HiveException("No support result Type");
+				return null;
+			} catch (HiveException e) {
+				e.printStackTrace();
 				throw new CompletionException(e);
-			} catch (Exception e) {
-				HiveException ex = new HiveException(e.getLocalizedMessage());
-				throw new CompletionException(ex);
 			}
 		});
+	}
+
+	private <T> T uploadFileImpl(String filePath, JsonNode params, Class<T> resultType) throws HiveException {
+		try {
+			String json = params.toString();
+			File file = new File(filePath);
+			RequestBody requestFile =
+					RequestBody.create(MediaType.parse("multipart/form-data"), file);
+			MultipartBody.Part body = MultipartBody.Part.createFormData("data", file.getName(), requestFile);
+
+			RequestBody metadata =
+					RequestBody.create(
+							MediaType.parse("application/json"), json);
+
+			Response<ResponseBody> response = this.connectionManager.getVaultApi()
+					.callScript(body, metadata)
+					.execute();
+			authHelper.checkResponseWithRetry(response);
+			return resultType.cast(ResponseHelper.toString(response));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new HiveException(e.getMessage());
+		}
+	}
+
+	private <T> T downloadFileImpl(String scriptName, JsonNode params, Class<T> resultType) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+
+			map.put("name", scriptName);
+			if (params != null)
+				map.put("params", params);
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getVaultApi()
+					.callScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
+					.execute();
+
+			if (response == null)
+				throw new HiveException(HiveException.ERROR);
+
+			authHelper.checkResponseWithRetry(response);
+			if (resultType.isAssignableFrom(Reader.class)) {
+				Reader reader = ResponseHelper.getToReader(response);
+				return resultType.cast(reader);
+			}
+			if (resultType.isAssignableFrom(InputStream.class)) {
+				InputStream inputStream = ResponseHelper.getInputStream(response);
+				return resultType.cast(inputStream);
+			}
+
+			throw new HiveException("No support result Type");
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
 	}
 }
