5249410fdfc2a03c3bdc72a506d11a7cfef2510c
xidaokun <xidaokun@elastos.org>
2020-11-23 11:03:44 +0800

    CU-57vewk remove wait time field

--- src/main/java/org/elastos/hive/payment/PaymentSettings.java
+++ src/main/java/org/elastos/hive/payment/PaymentSettings.java
@@ -7,20 +7,8 @@ import org.elastos.hive.database.Result;
 public class PaymentSettings extends Result<PaymentSettings> {
 	@JsonProperty("receivingELAAddress")
 	private String receivingELAAddress;
-	@JsonProperty("wait_payment_timeout")
-	private int waitPaymentTimeout;
-	@JsonProperty("wait_tx_timeout")
-	private int waitTxTimeout;
 
 	public String receivingELAAddress() {
 		return receivingELAAddress;
-	}
-
-	public int waitPaymentTimeout() {
-		return waitPaymentTimeout;
-	}
-
-	public int waitTxTimeout() {
-		return waitTxTimeout;
 	}
 }
