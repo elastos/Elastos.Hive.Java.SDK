cece5b40aa23156d4e26a62b57973352fce8dafa
xidaokun <xidaokun@elastos.org>
2020-11-23 11:43:13 +0800

    CU-ger7nf extend subclass GeneralCallConfig

--- src/main/java/org/elastos/hive/scripting/GeneralCallConfig.java
+++ src/main/java/org/elastos/hive/scripting/GeneralCallConfig.java
@@ -1,4 +1,30 @@
 package org.elastos.hive.scripting;
 
-public class GeneralCallConfig {
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Scripting general call config
+ */
+public class GeneralCallConfig extends CallConfig{
+
+	/**
+	 * Construction method
+	 *
+	 * @param params
+	 * @param resultType String, byte[], JsonNode, Reader
+	 */
+	public GeneralCallConfig(JsonNode params, Class resultType) {
+		this(null, params, resultType);
+	}
+
+	/**
+	 * Construction method
+	 *
+	 * @param appDid
+	 * @param params
+	 * @param resultType String, byte[], JsonNode, Reader
+	 */
+	public GeneralCallConfig(String appDid, JsonNode params, Class resultType) {
+		super(appDid, params, resultType);
+	}
 }
