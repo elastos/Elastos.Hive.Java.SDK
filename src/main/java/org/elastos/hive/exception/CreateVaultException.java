bcb867c5076416da17b68b9e4ed9e8dee3fb9bde
xidaokun <xidaokun@elastos.org>
2020-11-24 11:20:07 +0800

    CU-gern5c add create vault exception

--- src/main/java/org/elastos/hive/exception/CreateVaultException.java
+++ src/main/java/org/elastos/hive/exception/CreateVaultException.java
@@ -4,7 +4,7 @@ public class CreateVaultException extends RuntimeException {
 
 	private static final long serialVersionUID = 3332367416260335612L;
 
-	public static final String EXCEPTION = "Create vault failed";
+	public static final String EXCEPTION = "Vault has been created";
 
 	public CreateVaultException() {
 		super();
