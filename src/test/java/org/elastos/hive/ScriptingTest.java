d3745b46e76402256d5244d05c9fd1ba1ecf73ba
xidaokun <xidaokun@elastos.org>
2020-11-25 16:38:54 +0800

    CCU-57u0um improve create database test case

--- src/test/java/org/elastos/hive/ScriptingTest.java
+++ src/test/java/org/elastos/hive/ScriptingTest.java
@@ -1,6 +1,5 @@
 package org.elastos.hive;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
