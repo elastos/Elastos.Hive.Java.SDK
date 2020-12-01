ef9c518ffc1535949a1472d4b368510990a7d806
xidaokun <xidaokun@elastos.org>
2020-12-01 17:00:35 +0800

    CU-ejtctc automatic management of release version information script

--- src/main/java/org/elastos/hive/SdkVersion.java
+++ src/main/java/org/elastos/hive/SdkVersion.java
@@ -2,23 +2,33 @@ package org.elastos.hive;
 
 public class SdkVersion {
 
-	private static final String SDK_VERSION = "0.1.0";
+	private static final String SDK_VERSION = "alpha-v2.0.10";
 
-	private static final String LAST_COMMIT_ID = "0d421cc";
+	private static final String LAST_COMMIT_ID = "6908aaa";
+
+	private static final String COMMIT_BRANCH = "master";
 
 	/**
-	 * Get hive sdk version
-	 * @return
+	 * Get version
 	 */
 	public static String getVersion() {
 		return SDK_VERSION;
 	}
 
+
 	/**
-	 * Get the last commit ID on the hive sdk git repository
-	 * @return
+	 * Get last commitId
 	 */
 	public static String getLastCommitId() {
 		return LAST_COMMIT_ID;
 	}
+
+
+	/**
+	 * Get last commitId
+	 */
+	public static String getCommitBranch() {
+		return COMMIT_BRANCH;
+	}
+
 }
