8b2bb4a9b83949c5023d17d1c163673d4eaa36ed
xidaokun <xidaokun@elastos.org>
2020-12-03 10:17:25 +0800

    CU-guuwyq throw file not found exception

--- src/main/java/org/elastos/hive/ScriptingImpl.java
+++ src/main/java/org/elastos/hive/ScriptingImpl.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.elastos.hive.connection.ConnectionManager;
+import org.elastos.hive.exception.FileNotFoundException;
 import org.elastos.hive.exception.HiveException;
 import org.elastos.hive.files.UploadOutputStream;
 import org.elastos.hive.scripting.Condition;
@@ -165,7 +166,7 @@ class ScriptingImpl implements Scripting {
 
 	private <T> T downloadFileImpl(String name, JsonNode params, String appDid, Class<T> resultType) throws HiveException {
 		try {
-			JsonNode jsonNode = callScriptImpl(name, params, appDid, JsonNode.class);
+			JsonNode jsonNode = callDownloadScriptImpl(name, params, appDid, JsonNode.class);
 			JsonNode script = jsonNode.get(name);
 			String transactionId = script.get("transaction_id").textValue();
 			if(null != transactionId) {
@@ -191,6 +192,39 @@ class ScriptingImpl implements Scripting {
 			} else {
 				throw new HiveException("Can not get transaction id");
 			}
+		} catch (Exception e) {
+			throw new HiveException(e.getLocalizedMessage());
+		}
+	}
+
+	private <T> T callDownloadScriptImpl(String scriptName, JsonNode params, String appDid, Class<T> clazz) throws HiveException {
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("name", scriptName);
+
+			if(params!= null) map.put("params", params);
+
+			ObjectNode targetNode = JsonNodeFactory.instance.objectNode();
+			String ownerDid = this.authHelper.getOwnerDid();
+			if (null != ownerDid) {
+				targetNode.put("target_did", ownerDid);
+				if (null != appDid)
+					targetNode.put("target_app_did", appDid);
+				map.put("context", targetNode);
+			}
+
+			String json = JsonUtil.serialize(map);
+			Response<ResponseBody> response;
+
+			response = this.connectionManager.getScriptingApi()
+					.callScript(RequestBody.create(MediaType.parse("Content-Type, application/json"), json))
+					.execute();
+			int code = response.code();
+			if(404 == code) {
+				throw new FileNotFoundException(FileNotFoundException.EXCEPTION);
+			}
+			authHelper.checkResponseWithRetry(response);
+			return ResponseHelper.getValue(response, clazz);
 		} catch (Exception e) {
 			throw new HiveException(e.getLocalizedMessage());
 		}
