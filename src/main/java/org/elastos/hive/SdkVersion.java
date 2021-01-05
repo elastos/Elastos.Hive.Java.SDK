f900dbb08690eee8f15c0df091d535aeb2cb2e44
xidaokun <xidaokun@elastos.org>
2021-01-05 15:01:56 +0800

    CU-5vpwp8 add 404 fault not exist judgment

--- src/main/java/org/elastos/hive/SdkVersion.java
+++ src/main/java/org/elastos/hive/SdkVersion.java
@@ -2,9 +2,9 @@ package org.elastos.hive;
 
 public class SdkVersion {
 
-	private static final String SDK_VERSION = "2.0.0";
+	private static final String SDK_VERSION = "2.0.1";
 
-	private static final String LATEST_COMMIT_ID = "788cec9";
+	private static final String LATEST_COMMIT_ID = "fbb0a50";
 
 	private static final String COMMIT_BRANCH = "master";
 
