ad57b690522a0ea20019409b38a7699492ec7771
xidaokun <xidaokun@elastos.org>
2021-01-06 09:58:05 +0800

    CU-5vqvej remove commitId from SdkVersion

--- src/main/java/org/elastos/hive/SdkVersion.java
+++ src/main/java/org/elastos/hive/SdkVersion.java
@@ -4,23 +4,13 @@ public class SdkVersion {
 
 	private static final String SDK_VERSION = "2.0.4";
 
-	private static final String LATEST_COMMIT_ID = "788cec9";
-
-	private static final String COMMIT_BRANCH = "release-v2.0.0";
+	private static final String COMMIT_BRANCH = "master";
 
 	/**
 	 * Get version
 	 */
 	public static String getVersion() {
 		return SDK_VERSION;
-	}
-
-
-	/**
-	 * Get latest commitId
-	 */
-	public static String getLatestCommitId() {
-		return LATEST_COMMIT_ID;
 	}
 
 
