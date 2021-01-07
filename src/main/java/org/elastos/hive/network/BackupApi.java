b2dbb47b3b8f404517320b1e886cdc07b4ac3502
xidaokun <xidaokun@elastos.org>
2021-01-07 17:07:16 +0800

    CU-b4y18y add user interface definition and API interface definition

---
+++ src/main/java/org/elastos/hive/network/BackupApi.java
@@ -0,0 +1,23 @@
+package org.elastos.hive.network;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface BackupApi {
+
+	@POST(Constance.API_PATH + "/backup/state")
+	Call<ResponseBody> getState(@Body RequestBody body);
+
+	@POST(Constance.API_PATH + "/backup/save/to/node")
+	Call<ResponseBody> saveToNode(@Body RequestBody body);
+
+	@POST(Constance.API_PATH + "/backup/restore/from/node")
+	Call<ResponseBody> restoreFromNode(@Body RequestBody body);
+
+	@POST(Constance.API_PATH + "/backup/active/to/vault")
+	Call<ResponseBody> activeToVault(@Body RequestBody body);
+
+}
