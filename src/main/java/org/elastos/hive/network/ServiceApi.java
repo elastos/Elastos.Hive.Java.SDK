bbe0f9ef0b7dca529358492aa127adb5d335af19
xidaokun <xidaokun@elastos.org>
2021-01-11 15:31:57 +0800

    CU-b4y18y add service interface and backup authentication handler

--- src/main/java/org/elastos/hive/network/ServiceApi.java
+++ src/main/java/org/elastos/hive/network/ServiceApi.java
@@ -1,35 +1,31 @@
 package org.elastos.hive.network;
 
-import org.elastos.hive.backup.State;
-import org.elastos.hive.payment.UsingPlan;
-
-import java.util.concurrent.CompletableFuture;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 public interface ServiceApi {
 
-	/**
-	 * create free vault service
-	 * @return
-	 */
-	CompletableFuture<Boolean> create();
+	@POST(Constance.API_PATH + "/service/vault/create")
+	Call<ResponseBody> createVault(@Body RequestBody body);
 
+	@POST(Constance.API_PATH + "/service/vault/remove")
+	Call<ResponseBody> removeVault(@Body RequestBody body);
 
-	CompletableFuture<Boolean> remove();
+	@POST(Constance.API_PATH + "/service/vault/freeze")
+	Call<ResponseBody> freezeVault(@Body RequestBody body);
 
+	@POST(Constance.API_PATH + "/service/vault/freeze")
+	Call<ResponseBody> unfreezeVault(@Body RequestBody body);
 
-	CompletableFuture<Boolean> freeze();
+	@POST(Constance.API_PATH + "/service/vault")
+	Call<ResponseBody> getVaultServiceInfo(@Body RequestBody body);
 
+	@POST(Constance.API_PATH + "/service/vault_backup/create")
+	Call<ResponseBody> createBackupVault(@Body RequestBody body);
 
-	CompletableFuture<Boolean> unfreeze();
-
-
-	CompletableFuture<UsingPlan> getServiceInfo();
-
-
-	CompletableFuture<Boolean> createBackupService();
-
-
-	CompletableFuture<Object> getBackupServiceInfo();
-
-
+	@POST(Constance.API_PATH + "/service/vault_backup")
+	Call<ResponseBody> getBackupVaultInfo(@Body RequestBody body);
 }
