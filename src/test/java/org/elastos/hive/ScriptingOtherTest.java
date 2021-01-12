90bb39805974c77359b2e94bd931f1b3c6ec3455
xidaokun <xidaokun@elastos.org>
2021-01-12 10:29:10 +0800

    CU-4ttmvx improve code meaning

--- src/test/java/org/elastos/hive/ScriptingOtherTest.java
+++ src/test/java/org/elastos/hive/ScriptingOtherTest.java
@@ -6,14 +6,10 @@ import org.elastos.hive.didhelper.AppInstanceFactory;
 import org.elastos.hive.scripting.DbFindQuery;
 import org.elastos.hive.scripting.Executable;
 import org.elastos.hive.utils.JsonUtil;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -24,7 +20,7 @@ public class ScriptingOtherTest {
 	public void test00_callOtherScript() {
 		String scriptName = "get_groups";
 		CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() ->
-				AppInstanceFactory.getUser2().getVault().getScripting())
+				AppInstanceFactory.initConfig2().getVault().getScripting())
 				.thenComposeAsync(scripting ->
 						{
 							JsonNode filter = JsonUtil.deserialize("{\"friends\":\"$callScripter_did\"}");
@@ -33,7 +29,7 @@ public class ScriptingOtherTest {
 							return scripting.registerScript(scriptName, executable, false, false);
 						})
 				.thenApplyAsync(aBoolean ->
-						aBoolean ? AppInstanceFactory.getUser3().getVault().getScripting() : null)
+						aBoolean ? AppInstanceFactory.initConfig3().getVault().getScripting() : null)
 				.thenComposeAsync(scripting ->
 						scripting.callScript(scriptName, null, null, String.class))
 				.handle((BiFunction<String, Throwable, Boolean>) (s, throwable) ->
