d04b0da2f215d42d5f7ac64cc39a54a7bf123362
xidaokun <xidaokun@elastos.org>
2021-01-14 11:37:16 +0800

    improve service api pojo

--- src/main/java/org/elastos/hive/service/BackupUsingPlan.java
+++ src/main/java/org/elastos/hive/service/BackupUsingPlan.java
@@ -10,15 +10,43 @@ public class BackupUsingPlan extends Result<BackupUsingPlan> {
 	@JsonProperty("backup_using")
 	private String name;
 	@JsonProperty("max_storage")
-	private String maxStorage;
+	private long maxStorage;
 	@JsonProperty("use_storage")
-	private String useStorage;
+	private float useStorage;
 	@JsonProperty("modify_time")
 	private String modifyTime;
 	@JsonProperty("start_time")
 	private String startTime;
 	@JsonProperty("end_time")
 	private String endTime;
+
+	public String did() {
+		return did;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public long maxStorage() {
+		return maxStorage;
+	}
+
+	public float useStorage() {
+		return useStorage;
+	}
+
+	public String modifyTime() {
+		return modifyTime;
+	}
+
+	public String startTime() {
+		return startTime;
+	}
+
+	public String dndTime() {
+		return endTime;
+	}
 
 	public static BackupUsingPlan deserialize(String content) {
 		return deserialize(content, BackupUsingPlan.class);
