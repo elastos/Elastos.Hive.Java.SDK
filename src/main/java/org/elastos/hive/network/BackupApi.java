0310ab953b8a9120b7a7f06ba16719f18445a181
xidaokun <xidaokun@elastos.org>
2021-01-21 10:30:33 +0800

    fix exception

--- src/main/java/org/elastos/hive/network/BackupApi.java
+++ src/main/java/org/elastos/hive/network/BackupApi.java
@@ -4,11 +4,12 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface BackupApi {
 
-	@POST(Constance.API_PATH + "/backup/state")
+	@GET(Constance.API_PATH + "/backup/state")
 	Call<ResponseBody> getState();
 
 	@POST(Constance.API_PATH + "/backup/save_to_node")
