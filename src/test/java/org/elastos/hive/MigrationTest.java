1936d82297ccc572c9da45c46885c77aa5648fc5
xidaokun <xidaokun@elastos.org>
2021-01-22 14:08:40 +0800

    CU-5zq24x vault migration test case

--- src/test/java/org/elastos/hive/MigrationTest.java
+++ src/test/java/org/elastos/hive/MigrationTest.java
@@ -8,49 +8,69 @@ import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 @Ignore
 public class MigrationTest {
 	@Test
 	public void testMigration() {
-		managerApi.freezeVault().thenComposeAsync(aBoolean -> {
-			BackupAuthenticationHandler handler = new BackupAuthenticationHandler() {
-				@Override
-				public CompletableFuture<String> getAuthorization(String serviceDid) {
-					return CompletableFuture.supplyAsync(() ->
-							factory.getBackupVc(serviceDid));
-				}
+		CompletableFuture<Boolean> future = managerApi.freezeVault()
+				.thenComposeAsync(aBoolean -> {
+					BackupAuthenticationHandler handler = new BackupAuthenticationHandler() {
+						@Override
+						public CompletableFuture<String> getAuthorization(String serviceDid) {
+							return CompletableFuture.supplyAsync(() ->
+									factory.getBackupVc(serviceDid));
+						}
 
-				@Override
-				public String getTargetHost() {
-					return factory.getTargetHost();
-				}
+						@Override
+						public String getTargetHost() {
+							return factory.getTargetHost();
+						}
 
-				@Override
-				public String getTargetDid() {
-					return factory.getTargetDid();
-				}
-			};
-			return backupApi.save(handler);
-		}).thenApplyAsync(aBoolean -> {
-			for(;;) {
-				try {
-					Thread.sleep(10 * 1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				State state = backupApi.getState().join();
-				if(state == State.STOP) {
-					return true;
-				}
-			}
-		}).thenComposeAsync(aBoolean -> backupApi.active())
-		.handleAsync((aBoolean, throwable) -> (aBoolean && (null==throwable)));
+						@Override
+						public String getTargetDid() {
+							return factory.getTargetDid();
+						}
+					};
+					return backupApi.save(handler);
+				}).thenApplyAsync(aBoolean -> {
+					for (; ; ) {
+						try {
+							Thread.sleep(10 * 1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						State state = backupApi.getState().join();
+						if (state == State.STOP) {
+							return true;
+						}
+					}
+				}).thenComposeAsync(aBoolean -> backupApi.active())
+				.handleAsync((aBoolean, throwable) -> {
+					if (null != throwable) {
+						throwable.printStackTrace();
+					}
+					return (aBoolean && (null == throwable));
+				});
+
+
+		try {
+			assertTrue(future.get());
+			assertTrue(future.isCompletedExceptionally() == false);
+			assertTrue(future.isDone());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 
 	private static AppInstanceFactory factory;
 	static Backup backupApi;
 	private static Manager managerApi;
+
 	@BeforeClass
 	public static void setUp() {
 		factory = AppInstanceFactory.configSelector();
