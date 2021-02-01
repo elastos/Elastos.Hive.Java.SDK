5f5c47aecdb686de4beae9649e249a96ea4285bc
xidaokun <xidaokun@elastos.org>
2021-02-01 13:17:59 +0800

    improve hive url test case

--- src/test/java/org/elastos/hive/HiveURLTest.java
+++ src/test/java/org/elastos/hive/HiveURLTest.java
@@ -1,14 +1,20 @@
 package org.elastos.hive;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.elastos.hive.didhelper.AppInstanceFactory;
 import org.elastos.hive.scripting.AggregatedExecutable;
 import org.elastos.hive.scripting.Executable;
 import org.elastos.hive.scripting.HashExecutable;
 import org.elastos.hive.scripting.PropertiesExecutable;
+import org.elastos.hive.scripting.UploadExecutable;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.Assert.assertTrue;
@@ -17,9 +23,9 @@ import static org.junit.Assert.fail;
 @Ignore
 public class HiveURLTest {
 
-//	private final String scriptUrl = "hive://did:elastos:icXtpDnZRSDrjmD5NQt6TYSphFRqoo2q6n@appId/scripting/get_file_info?params={\"group_id\":{\"$oid\":\"5f497bb83bd36ab235d82e6a\"},\"path\":\"test.txt\"}";
+	private final String scriptUrl = "hive://did:elastos:icXtpDnZRSDrjmD5NQt6TYSphFRqoo2q6n@appId/scripting/get_file_info?params={\"group_id\":{\"$oid\":\"5f497bb83bd36ab235d82e6a\"},\"path\":\"test.txt\"}";
 
-	private final String scriptUrl = "hive://did:elastos:icXtpDnZRSDrjmD5NQt6TYSphFRqoo2q6n@appId/scripting/downloadScript?params={'key':'test'}";
+//	private final String scriptUrl = "hive://did:elastos:icXtpDnZRSDrjmD5NQt6TYSphFRqoo2q6n@appId/scripting/downloadScript?params={'key':'test'}";
 
 	@Test
 	public void testGetHiveURL() {
@@ -31,6 +37,49 @@ public class HiveURLTest {
 			assertTrue(future.isCompletedExceptionally() == false);
 			assertTrue(future.isDone());
 		} catch (Exception e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void uploadFile() {
+		Executable executable = new UploadExecutable("upload_file", "$params.path", true);
+		CompletableFuture<Boolean> future = scriptingApi.registerScript("upload_file", executable, false, false)
+				.thenComposeAsync(aBoolean -> {
+
+					String scriptName = "upload_file";
+					String metadata = "{\"group_id\":{\"$oid\":\"5f8d9dfe2f4c8b7a6f8ec0f1\"},\"path\":\"test.txt\"}";
+					JsonNode params = null;
+					try {
+						ObjectMapper objectMapper = new ObjectMapper();
+						params = objectMapper.readTree(metadata);
+					} catch (Exception e) {
+						fail();
+					}
+
+					return scriptingApi.callScript(scriptName, params, "appId", JsonNode.class)
+							.thenComposeAsync(jsonNode -> {
+								String transactionId = jsonNode.get(scriptName).get("transaction_id").textValue();
+								return scriptingApi.uploadFile(transactionId, Writer.class);
+							}).handle((writer, ex) -> {
+								if(null != writer) {
+									Utils.fileWrite(textLocalPath, writer);
+									try {
+										writer.close();
+									} catch (IOException e) {
+										e.printStackTrace();
+									}
+								}
+								return ex == null;
+							});
+				});
+
+		try {
+			assertTrue(future.get());
+			assertTrue(future.isCompletedExceptionally() == false);
+			assertTrue(future.isDone());
+		} catch (Exception e) {
+			e.printStackTrace();
 			fail();
 		}
 	}
@@ -60,11 +109,14 @@ public class HiveURLTest {
 
 	private static Client client;
 	private static Scripting scriptingApi;
+	private static String textLocalPath;
 
 	@BeforeClass
 	public static void setUp() {
 		client = AppInstanceFactory.configSelector().getClient();
 		scriptingApi = AppInstanceFactory.configSelector().getVault().getScripting();
+		String localRootPath = System.getProperty("user.dir") + "/src/test/resources/";
+		textLocalPath = localRootPath + "test.txt";
 	}
 
 }
