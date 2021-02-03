40c29ad54ce57e582f066d1cc38c3b8b3f750324
xidaokun <xidaokun@elastos.org>
2021-02-03 12:34:25 +0800

    CU-69zc0n download file by hive url
    (cherry picked from commit 7fb32f822d4bb4d3cc268cfb2ca0a817eba1ef85)

---
+++ src/main/java/org/elastos/hive/UrlInfo.java
@@ -0,0 +1,40 @@
+package org.elastos.hive;
+
+class UrlInfo {
+	private String targetDid;
+	private String appDid;
+	private String scriptName;
+	private String params;
+
+	public String getTargetDid() {
+		return targetDid;
+	}
+
+	public void setTargetDid(String targetDid) {
+		this.targetDid = targetDid;
+	}
+
+	public String getAppDid() {
+		return appDid;
+	}
+
+	public void setAppDid(String appDid) {
+		this.appDid = appDid;
+	}
+
+	public String getScriptName() {
+		return scriptName;
+	}
+
+	public void setScriptName(String scriptName) {
+		this.scriptName = scriptName;
+	}
+
+	public String getParams() {
+		return params;
+	}
+
+	public void setParams(String params) {
+		this.params = params;
+	}
+}
