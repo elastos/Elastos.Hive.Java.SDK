2e59600d5a7e12490aedab53e14e414bd29ac017
xidaokun <xidaokun@elastos.org>
2021-02-04 16:38:31 +0800

    CU-6bxy5p add mock application and activity

---
+++ src/test/java/org/elastos/hive/MockApplication.java
@@ -0,0 +1,22 @@
+package org.elastos.hive;
+
+/**
+ * Application of simulation upper layer
+ */
+public class MockApplication implements Activity {
+
+	@Override
+	public void onCreate(ApplicationContext context) {
+
+	}
+
+	@Override
+	public void onResume(ApplicationContext context) {
+
+	}
+
+	@Override
+	public void onDestroy(ApplicationContext context) {
+
+	}
+}
