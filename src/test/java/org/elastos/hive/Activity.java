7d2f58cb7d136246fee876a78c05c782e624fdc8
xidaokun <xidaokun@elastos.org>
2021-02-04 16:38:31 +0800

    CU-6bxy5p add mock application and activity

---
+++ src/test/java/org/elastos/hive/Activity.java
@@ -0,0 +1,11 @@
+package org.elastos.hive;
+
+public interface Activity {
+
+	void onCreate(ApplicationContext context);
+
+	void onResume(ApplicationContext context);
+
+	void onDestroy(ApplicationContext context);
+
+}
