ac897eac1532ae2b34c224f00c0758655414e0fd
xidaokun <xidaokun@elastos.org>
2021-02-05 10:48:20 +0800

    CU-6bxy5p improve application and Activity

---
+++ src/test/java/org/elastos/hive/MainActivity.java
@@ -0,0 +1,11 @@
+package org.elastos.hive;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onResume(ApplicationContext context) {
+		super.onResume(context);
+
+	}
+
+}
