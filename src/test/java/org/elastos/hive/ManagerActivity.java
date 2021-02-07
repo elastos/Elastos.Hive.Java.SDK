56efd872a5f23f5a3b7c0a7921b6306fde7deab7
xidaokun <xidaokun@elastos.org>
2021-02-07 17:40:11 +0800

    CU-6bxy5p add ManagerActivity and VaultActivity

---
+++ src/test/java/org/elastos/hive/ManagerActivity.java
@@ -0,0 +1,15 @@
+package org.elastos.hive;
+
+public class ManagerActivity extends Activity {
+
+	@Override
+	protected void onCreate(ApplicationContext context) {
+		super.onCreate(context);
+	}
+
+
+	@Override
+	protected void onResume(ApplicationContext context) {
+		super.onResume(context);
+	}
+}
