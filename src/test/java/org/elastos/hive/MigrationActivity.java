bbb90806cc14e620df322979700fcbb3465dadae
xidaokun <xidaokun@elastos.org>
2021-02-08 11:46:54 +0800

    CU-6bxy5p int node config and user config

--- src/test/java/org/elastos/hive/MigrationActivity.java
+++ src/test/java/org/elastos/hive/MigrationActivity.java
@@ -2,12 +2,12 @@ package org.elastos.hive;
 
 public class MigrationActivity extends Activity {
 	@Override
-	protected void onCreate(ApplicationContext context) {
+	protected void onCreate(Application context) {
 		super.onCreate(context);
 	}
 
 	@Override
-	protected void onResume(ApplicationContext context) {
+	protected void onResume(Application context) {
 		super.onResume(context);
 	}
 }
