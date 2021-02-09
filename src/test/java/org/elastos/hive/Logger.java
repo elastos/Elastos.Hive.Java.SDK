bba3260a03dad0778ec75db6e930da5b555153e9
xidaokun <xidaokun@elastos.org>
2021-02-09 16:26:18 +0800

    Finishing logger color paper

--- src/test/java/org/elastos/hive/Logger.java
+++ src/test/java/org/elastos/hive/Logger.java
@@ -1,5 +1,7 @@
 package org.elastos.hive;
 
+import org.elastos.hive.scripting.Executable;
+
 public class Logger {
 
 	public static void hive() {
@@ -16,10 +18,30 @@ public class Logger {
 	}
 
 	public static void controllerStart(String tag) {
-		System.out.println("\033[1;32m" + "=====>" + tag + "  start" + "\033[0m");
+		information("=====>" + tag + "  start");
 	}
 
 	public static void controllerEnd(String tag) {
-		System.out.println("\033[1;32m" + "=====>" + tag + "  end" + "\033[0m");
+		information("=====>" + tag + "  end");
+	}
+
+	public static void error(String error) {
+		System.out.println("\033[7;31m" + error + "\033[0m");
+	}
+
+	public static void exception(String exception) {
+		System.out.println("\033[0;31m" + exception + "\033[0m");
+	}
+
+	public static void exception(Exception exception) {
+		System.out.println("\033[0;31m" + exception.getLocalizedMessage() + "\033[0m");
+	}
+
+	public static void information(String information) {
+		System.out.println("\033[0;32m" + information + "\033[0m");
+	}
+
+	public static void debug(String message) {
+		System.out.println("\033[0;36m" + message + "\033[0m");
 	}
 }
