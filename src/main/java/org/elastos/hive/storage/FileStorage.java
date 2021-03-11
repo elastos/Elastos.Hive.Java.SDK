7260b7f578d6542ad7cba3a68e35dc8811ee4d97
stiartsly <stiartsly@gmail.com>
2021-03-11 17:14:03 +0800

    Added Data storage interface and it's extends FileStorage

---
+++ src/main/java/org/elastos/hive/storage/FileStorage.java
@@ -0,0 +1,50 @@
+package org.elastos.hive.storage;
+
+public class FileStorage implements DataStorage {
+	@Override
+	public String loadBackupCredential(String serviceDid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadSigninCredential() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadAccessToken(String serviceDid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadAccessTokenByAddress(String providerAddress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void storeBackupCredential(String serviceDid, String credential) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void storeSigninCredential(String credential) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void storeAccessToken(String serviceDid, String accessToken) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void storeAccessTokenByAddress(String serviceDid, String providerAddress) {
+		// TODO Auto-generated method stub
+	}
+}
