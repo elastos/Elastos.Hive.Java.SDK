893e7f50e96e7128ec6149ff6992de472f92505e
stiartsly <stiartsly@gmail.com>
2021-03-18 22:01:58 +0800

    Minor format improvements

--- src/main/java/org/elastos/hive/auth/TokenResolver.java
+++ src/main/java/org/elastos/hive/auth/TokenResolver.java
@@ -1,10 +1,8 @@
 package org.elastos.hive.auth;
 
 import org.elastos.hive.exception.HiveException;
-import org.jetbrains.annotations.NotNull;
 
 public interface TokenResolver {
-	@NotNull
 	AuthToken getToken() throws HiveException;
 	void invlidateToken();
 	void setNextResolver(TokenResolver resolver);
