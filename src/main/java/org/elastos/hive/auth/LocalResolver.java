a2a5fb16ec32a8417f5541b3d29d568bf44b967e
stiartsly <stiartsly@gmail.com>
2021-03-18 17:50:59 +0800

    Improve internal method implementation for localResolver to acquire token

--- src/main/java/org/elastos/hive/auth/LocalResolver.java
+++ src/main/java/org/elastos/hive/auth/LocalResolver.java
@@ -12,48 +12,37 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class LocalResolver implements TokenResolver {
-	private static final String TOKEN_FOLDER = "/token";
+	private static final String TOKEN_FOLDER = "/tokens";
 
 	private String tokenPath;
 	private TokenResolver nextResolver;
 	private AuthToken token;
 
-	public LocalResolver(String ownerDid, String providerAddress, String rootPath) {
-		String rootDir = rootPath + TOKEN_FOLDER;
+	public LocalResolver(String ownerDid, String providerAddress, String cacheDir) {
+		String rootDir = cacheDir + TOKEN_FOLDER;
 		File root = new File(rootDir);
+
 		if (!root.exists() && !root.mkdirs()) {
 			throw new HiveSdkException("Cannot create token root path.");
 		}
 		this.tokenPath = String.format("%s/%s", rootDir, CryptoUtil.getSHA256(ownerDid + providerAddress));
 	}
 
-	@NotNull
 	@Override
 	public AuthToken getToken() throws HiveException {
-		if (this.token != null)
-			return this.token;
-		AuthToken authToken = getLocalToken();
-		if (authToken == null) {
-			authToken = nextResolver.getToken();
-			saveToken(authToken);
-		}
-		return authToken;
-	}
+		if (token == null)
+			token = restoreToken();
 
-	private AuthToken getLocalToken() throws HiveException {
-		if (!Files.exists(Paths.get(this.tokenPath))) {
-			return null;
+		if (token == null) {
+			token = nextResolver.getToken();
+			saveToken(token);
 		}
-		try {
-			String content = new String(Files.readAllBytes(Paths.get(this.tokenPath)));
-			LogUtil.d("Load token from local file.");
-			return new Gson().fromJson(content, AuthToken.class);
-		} catch (IOException e) {
-			throw new HiveException("Failed to load token from local file.");
-		}
+
+		return token;
 	}
 
 	@Override
@@ -61,17 +50,38 @@ public class LocalResolver implements TokenResolver {
 		saveToken(this.token);
 	}
 
-	private void saveToken(AuthToken token) throws HiveException {
-		try {
-			Files.write(Paths.get(this.tokenPath), new Gson().toJson(token).getBytes(StandardCharsets.UTF_8));
-			this.token = token;
-		} catch (IOException e) {
-			throw new HiveException("Failed to save token to local file.");
-		}
-	}
-
 	@Override
 	public void setNextResolver(TokenResolver resolver) {
 		this.nextResolver = resolver;
+	}
+
+	private AuthToken restoreToken() {
+		Path path = Paths.get(tokenPath);
+
+		if (!Files.exists(path))
+			return null;
+
+		try {
+			LogUtil.d("Restore access token  from local cache");
+			return new Gson().fromJson(new String(Files.readAllBytes(path)), AuthToken.class);
+		} catch (IOException e) {
+			LogUtil.e("Failed to restore access token from local cache");
+			return null;
+		}
+	}
+
+	private void saveToken(AuthToken token) {
+		Path path = Paths.get(tokenPath);
+
+		if (!Files.exists(path)) {
+			// TODO: create path.
+		}
+
+		try {
+			Files.write(path, new Gson().toJson(token).getBytes(StandardCharsets.UTF_8));
+		} catch (IOException e) {
+			LogUtil.e("Failed to save access token to local cache");
+		}
+
 	}
 }
