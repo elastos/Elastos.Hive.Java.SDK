da51aa1adc3db538f51846f04c692cfbb0b9909a
xidaokun <xidaokun@elastos.org>
2021-03-23 11:08:11 +0800

    CU-6zrz5y Sort out test cases

--- src/test/java/org/elastos/hive/BackupSubscriptionTest.java
+++ src/test/java/org/elastos/hive/BackupSubscriptionTest.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+@Ignore
 public class BackupSubscriptionTest {
 
 	private static BackupSubscription subscription;
