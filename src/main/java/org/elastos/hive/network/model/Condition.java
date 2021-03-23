ec969ed0b2da283afd52294972b316bd2d709e46
Fred Chen <chentj070921@yeah.net>
2021-03-23 09:41:22 +0800

    Add file upload test case for scripting service.

--- src/main/java/org/elastos/hive/network/model/Condition.java
+++ src/main/java/org/elastos/hive/network/model/Condition.java
@@ -1,10 +1,21 @@
 package org.elastos.hive.network.model;
 
-public class Condition extends Executable {
+import com.google.gson.annotations.SerializedName;
+
+public class Condition {
 	public static final String TYPE_AND = "and";
 	public static final String TYPE_OR = "or";
 
+	@SerializedName("name")
+	private String name;
+	@SerializedName("type")
+	private String type;
+	@SerializedName("body")
+	private Object body;
+
 	public Condition(String name, String type, Object body) {
-		super(name, type, body);
+		this.name = name;
+		this.type = type;
+		this.body = body;
 	}
 }
