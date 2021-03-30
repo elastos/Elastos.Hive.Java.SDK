2c9ddf54a5eb56550838ce393bf093c946c088bb
Fred Chen <chentj070921@yeah.net>
2021-03-30 17:38:50 +0800

    Add pubsub service interface definition which will be discussed later.

--- src/main/java/org/elastos/hive/vault/PubSubServiceRender.java
+++ src/main/java/org/elastos/hive/vault/PubSubServiceRender.java
@@ -1,11 +1,54 @@
 package org.elastos.hive.vault;
 
 import org.elastos.hive.Vault;
+import org.elastos.hive.network.model.ChannelMessage;
 import org.elastos.hive.service.PubSubService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 class PubSubServiceRender implements PubSubService {
 	public PubSubServiceRender(Vault vault) {
-
+		// TODO:
 	}
 
+	@Override
+	public CompletableFuture<Boolean> publish(String channelName) {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<Boolean> remove(String channelName) {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<List<String>> getPublishedChannels() {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<List<String>> getSubscribedChannels() {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<Boolean> subscribe(String channelName, String pubDid, String pubAppId) {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<Boolean> unsubscribe(String channelName, String pubDid, String pubAppId) {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<Boolean> push(String channelName, String message) {
+		return null;
+	}
+
+	@Override
+	public CompletableFuture<List<ChannelMessage>> pop(String channelName, String pubDid, String pubAppId, int limit) {
+		return null;
+	}
 }
