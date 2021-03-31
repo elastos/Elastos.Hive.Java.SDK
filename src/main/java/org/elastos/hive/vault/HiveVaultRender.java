0e9ad3b7c881e5fe511980a889538b2a1e17e207
Fred Chen <chentj070921@yeah.net>
2021-03-31 14:15:17 +0800

    Refactor backup service exception handling.

--- src/main/java/org/elastos/hive/vault/HiveVaultRender.java
+++ src/main/java/org/elastos/hive/vault/HiveVaultRender.java
@@ -3,20 +3,26 @@ package org.elastos.hive.vault;
 import org.elastos.hive.Vault;
 import org.elastos.hive.connection.ConnectionManager;
 import org.elastos.hive.exception.HiveException;
+import org.elastos.hive.exception.HttpFailedException;
 import org.elastos.hive.exception.NoEnoughSpaceException;
 import org.elastos.hive.exception.VaultLockedException;
-import org.elastos.hive.network.response.HiveResponseBody;
 
 import javax.security.sasl.AuthenticationException;
 
 public abstract class HiveVaultRender {
+    private Vault vault;
     private ConnectionManager connectionManager;
 
     protected HiveVaultRender(Vault vault) {
+        this.vault = vault;
         this.connectionManager = vault.getAppContext().getConnectionManager();
     }
 
-    public ConnectionManager getConnectionManager() {
+    protected Vault getVault() {
+        return this.vault;
+    }
+
+    protected ConnectionManager getConnectionManager() {
         return this.connectionManager;
     }
 
@@ -30,13 +36,19 @@ public abstract class HiveVaultRender {
      * @return expect exception already defined.
      */
     protected Exception convertException(Exception e) {
-        if (HiveResponseBody.msgContainsCode(e.getMessage(), 401))
-            return new AuthenticationException();
-        else if (HiveResponseBody.msgContainsCode(e.getMessage(), 423))
-            return new VaultLockedException();
-        else if (HiveResponseBody.msgContainsCode(e.getMessage(), 452))
-            return new NoEnoughSpaceException();
-        else
-            return new HiveException(e.getMessage());
+        if (e instanceof HttpFailedException) {
+            HttpFailedException ex = (HttpFailedException) e;
+            switch (ex.getCode()) {
+                case 401:
+                    return new AuthenticationException();
+                case 423:
+                    return new VaultLockedException();
+                case 452:
+                    return new NoEnoughSpaceException();
+                default:
+                    break;
+            }
+        }
+        return new HiveException(e.getMessage());
     }
 }
