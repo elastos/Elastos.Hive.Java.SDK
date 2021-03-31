d5b5e9eae8e199e670d0bfa51214d64ffc5a0d43
Fred Chen <chentj070921@yeah.net>
2021-03-31 17:44:00 +0800

    Refactor all test cases.

--- src/test/java/org/elastos/hive/VaultPaymentTest.java
+++ src/test/java/org/elastos/hive/VaultPaymentTest.java
@@ -1,22 +1,20 @@
 package org.elastos.hive;
 
+import com.google.common.base.Throwables;
+import org.elastos.did.exception.DIDException;
 import org.elastos.hive.config.TestData;
+import org.elastos.hive.exception.HiveException;
 import org.elastos.hive.payment.Order;
 import org.elastos.hive.payment.PricingPlan;
 import org.elastos.hive.payment.Receipt;
 import org.elastos.hive.service.PaymentService;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class VaultPaymentTest {
+class VaultPaymentTest {
     private static final String PRICING_PLAN_NAME = "Rookie";
 
     private static PaymentService paymentService;
@@ -26,66 +24,46 @@ public class VaultPaymentTest {
         try {
             TestData testData = TestData.getInstance();
             paymentService = new VaultSubscription(testData.getAppContext(), testData.getOwnerDid(), testData.getProviderAddress());
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
+        } catch (HiveException | DIDException e) {
+            Assertions.fail(Throwables.getStackTraceAsString(e));
         }
     }
 
     @Test
     @org.junit.jupiter.api.Order(1)
-    public void testGetPricingPlanList() {
+    void testGetPricingPlanList() {
         try {
-            List<PricingPlan> plans = paymentService.getPricingPlanList().exceptionally(e->{
-                fail();
-                return null;
-            }).get();
-            assertNotNull(plans);
-            assertFalse(plans.isEmpty());
+            List<PricingPlan> plans = paymentService.getPricingPlanList().get();
+            Assertions.assertNotNull(plans);
+            Assertions.assertFalse(plans.isEmpty());
         } catch (Exception e) {
-            e.printStackTrace();
-            fail();
+            Assertions.fail(Throwables.getStackTraceAsString(e));
         }
     }
 
     @Test
     @org.junit.jupiter.api.Order(2)
-    public void testGetPricingPlan() {
+    void testGetPricingPlan() {
         try {
-            PricingPlan plan = paymentService.getPricingPlan(PRICING_PLAN_NAME).exceptionally(e->{
-                fail();
-                return null;
-            }).get();
-            assertNotNull(plan);
+            PricingPlan plan = paymentService.getPricingPlan(PRICING_PLAN_NAME).get();
+            Assertions.assertNotNull(plan);
         } catch (Exception e) {
-            e.printStackTrace();
-            fail();
+            Assertions.fail(Throwables.getStackTraceAsString(e));
         }
     }
 
     @Test
     @org.junit.jupiter.api.Order(3)
-    public void testOrderProcess() {
+    void testOrderProcess() {
         try {
-            Order order = paymentService.placeOrder(PRICING_PLAN_NAME).exceptionally(e->{
-                fail();
-                return null;
-            }).get();
-            assertNotNull(order);
-            order = paymentService.getOrder(order.getOrderId()).exceptionally(e->{
-                fail();
-                return null;
-            }).get();
-            assertNotNull(order);
-            Receipt receipt = paymentService.payOrder(order.getOrderId(), Collections.emptyList())
-                    .exceptionally(e -> {
-                        fail();
-                        return null;
-                    }).get();
-            assertNotNull(receipt);
+            Order order = paymentService.placeOrder(PRICING_PLAN_NAME).get();
+            Assertions.assertNotNull(order);
+            order = paymentService.getOrder(order.getOrderId()).get();
+            Assertions.assertNotNull(order);
+            Receipt receipt = paymentService.payOrder(order.getOrderId(), Collections.emptyList()).get();
+            Assertions.assertNotNull(receipt);
         } catch (Exception e) {
-            e.printStackTrace();
-            fail();
+            Assertions.fail(Throwables.getStackTraceAsString(e));
         }
     }
 }
