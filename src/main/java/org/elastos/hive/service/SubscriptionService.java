26af31cb83173fd589a21ffc816579a77248381e
Fred Chen <chentj070921@yeah.net>
2021-04-02 11:54:23 +0800

    Add comments for subscription service interface.

--- src/main/java/org/elastos/hive/service/SubscriptionService.java
+++ src/main/java/org/elastos/hive/service/SubscriptionService.java
@@ -2,14 +2,46 @@ package org.elastos.hive.service;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Subscription is for using the functions of the vault service.
+ * Before using it, subscribe request MUST be called.
+ *
+ * @param <T> Vault information for specific vault server.
+ */
 public interface SubscriptionService<T> {
+	/**
+	 * Subscript for using vault relating service.
+	 *
+	 * @param pricingPlan Pricing plan name for using the vault service.
+	 * @return Vault service information.
+	 */
 	CompletableFuture<T> subscribe(String pricingPlan);
 
+	/**
+	 * Unsubscript vault service. After this, user can't use it.
+	 *
+	 * @return Void
+	 */
 	CompletableFuture<Void> unsubscribe();
 
+	/**
+	 * Activate vault service for writing permission.
+	 *
+	 * @return Void.
+	 */
 	CompletableFuture<Void> activate();
 
+	/**
+	 * Remove writing permission for vault service.
+	 *
+	 * @return Void
+	 */
 	CompletableFuture<Void> deactivate();
 
+	/**
+	 * Check the subscription information.
+	 *
+	 * @return Subscription information for vault relating service.
+	 */
 	CompletableFuture<T> checkSubscription();
 }
