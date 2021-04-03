184c5729cde38aa185d2d7bd22b14a9eed0b5c2f
Fred Chen <chentj070921@yeah.net>
2021-04-03 16:53:11 +0800

    Optimize vault active process and support backup valut info access.

--- src/main/java/org/elastos/hive/network/BackupApi.java
+++ src/main/java/org/elastos/hive/network/BackupApi.java
@@ -12,15 +12,27 @@ import retrofit2.http.POST;
 
 public interface BackupApi {
 
+	/**
+	 * Current backup process status on node side.
+	 */
 	@GET(BaseApi.API_VERSION + "/backup/state")
 	Call<BackupStateResponseBody> getState();
 
+	/**
+	 * Save the database and files data to backup node server from vault node server.
+	 */
 	@POST(BaseApi.API_VERSION + "/backup/save_to_node")
 	Call<HiveResponseBody> saveToNode(@Body BackupSaveRequestBody body);
 
+	/**
+	 * Just restore data to vault without replace the exist one.
+	 */
 	@POST(BaseApi.API_VERSION + "/backup/restore_from_node")
 	Call<HiveResponseBody> restoreFromNode(@Body BackupRestoreRequestBody body);
 
+	/**
+	 * Replace restore data to the current vault and unfreeze it.
+	 */
 	@POST(BaseApi.API_VERSION + "/backup/activate_to_vault")
 	Call<HiveResponseBody> activeToVault(@Body EmptyRequestBody body);
 
