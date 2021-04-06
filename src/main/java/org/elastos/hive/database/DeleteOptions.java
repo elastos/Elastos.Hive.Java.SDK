3e88e17803cf5c3a95186ccacd40fd7b9327706c
Fred Chen <chentj070921@yeah.net>
2021-04-06 13:40:19 +0800

    Refine database service test cases.

--- src/main/java/org/elastos/hive/database/DeleteOptions.java
+++ src/main/java/org/elastos/hive/database/DeleteOptions.java
@@ -1,26 +1,7 @@
 package org.elastos.hive.database;
 
-import java.util.List;
-
+/**
+ * Node not support this delete options.
+ */
 public class DeleteOptions {
-	private Collation collation;
-	private List<Index> hint;
-
-	public DeleteOptions setCollation(Collation collation) {
-		this.collation = collation;
-		return this;
-	}
-
-	public DeleteOptions setHint(List<Index> hint) {
-		this.hint = hint;
-		return this;
-	}
-
-	public Collation getCollation() {
-		return collation;
-	}
-
-	public List<Index> getHint() {
-		return hint;
-	}
 }
--- src/main/java/org/elastos/hive/network/request/InsertDocRequestBody.java
+++ src/main/java/org/elastos/hive/network/request/InsertDocRequestBody.java
@@ -1,13 +1,13 @@
 package org.elastos.hive.network.request;
 
-import org.elastos.hive.database.InsertOptions;
+import org.elastos.hive.database.InsertOneOptions;
 import org.elastos.hive.network.model.KeyValueDict;
 
 public class InsertDocRequestBody extends CreateCollectionRequestBody {
     private final KeyValueDict document;
-    private final InsertOptions options;
+    private final InsertOneOptions options;
 
-    public InsertDocRequestBody(String name, KeyValueDict document, InsertOptions options) {
+    public InsertDocRequestBody(String name, KeyValueDict document, InsertOneOptions options) {
         super(name);
         this.document = document;
         this.options = options;
