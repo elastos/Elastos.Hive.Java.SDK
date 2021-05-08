278f65295958d77d3f4c14a52049a2389ea011e5
stiartsly <stiartsly@gmail.com>
2021-05-08 14:04:25 +0800

    Add ScriptRunner implmenting from ScriptingInvocationService

--- src/main/java/org/elastos/hive/ScriptRunner.java
+++ src/main/java/org/elastos/hive/ScriptRunner.java
@@ -1,11 +1,13 @@
 package org.elastos.hive;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
+import org.elastos.hive.service.ScriptingInvocationService;
 import org.elastos.hive.vault.ScriptingServiceRender;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ScriptRunner extends ServiceEndpoint {
+public class ScriptRunner extends ServiceEndpoint implements ScriptingInvocationService {
 	private ScriptingServiceRender scriptingServiceRender;
 
 	public ScriptRunner(AppContext context, String providerAddress) {
@@ -51,6 +53,7 @@ public class ScriptRunner extends ServiceEndpoint {
 	 * @param <T>           Write, OutputStream
 	 * @return 				Write, OutputStream
 	 */
+	@Override
 	public <T> CompletableFuture<T> downloadFile(String transactionId, Class<T> resultType) {
 		return scriptingServiceRender.downloadFile(transactionId, resultType);
 	}
@@ -63,7 +66,14 @@ public class ScriptRunner extends ServiceEndpoint {
 	 * @param <T>        Reader, InputStream
 	 * @return			 Reader, InputStream
 	 */
+	@Override
 	public <T> CompletableFuture<T> uploadFile(String transactionId, Class<T> resultType) {
 		return scriptingServiceRender.uploadFile(transactionId, resultType);
+	}
+
+	@Override
+	public <T> CompletableFuture<T> callScript(String name, JsonNode params, String appDid, Class<T> resultType) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
