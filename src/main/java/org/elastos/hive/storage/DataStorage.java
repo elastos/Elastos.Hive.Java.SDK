1df0dfbbeef73efa6e2e490e03ce6090af4beceb
Fred Chen <chentj070921@yeah.net>
2021-05-11 19:27:25 +0800

    CU-8dwc8g - Implement FileStorage class and refine DataStorage interface.

--- src/main/java/org/elastos/hive/storage/DataStorage.java
+++ src/main/java/org/elastos/hive/storage/DataStorage.java
@@ -3,11 +3,40 @@ package org.elastos.hive.storage;
 public interface DataStorage {
 	String loadBackupCredential(String serviceDid);
 	String loadSignInCredential();
+
+	/**
+	 * Load access token by serviceDid which can be used for authorization.
+	 * @param serviceDid service did
+	 * @return access token
+	 */
 	String loadAccessToken(String serviceDid);
+
+	/**
+	 * Load access token by provider address which can be used for authorization.
+	 * @param providerAddress provider address to access
+	 * @return access token
+	 */
 	String loadAccessTokenByAddress(String providerAddress);
 
 	void storeBackupCredential(String serviceDid, String credential);
 	void storeSignInCredential(String credential);
+
+	/**
+	 * Store access token to data storage by service did.
+	 * @param serviceDid service did
+	 * @param accessToken access token to be stored
+	 */
 	void storeAccessToken(String serviceDid, String accessToken);
-	void storeAccessTokenByAddress(String serviceDid, String providerAddress);
+
+	/**
+	 * Store access token to data storage by provider address.
+	 * @param providerAddress provider address
+	 * @param accessToken access token
+	 */
+	void storeAccessTokenByAddress(String providerAddress, String accessToken);
+
+	void clearBackupCredential(String serviceDid);
+	void clearSignInCredential();
+	void clearAccessToken(String serviceDid);
+	void clearAccessTokenByAddress(String providerAddress);
 }
