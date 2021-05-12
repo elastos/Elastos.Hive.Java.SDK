8df86944eac04a9feddafd8dc3faca43b5ea2c99
stiartsly <stiartsly@gmail.com>
2021-05-12 23:03:30 +0800

    Rename nodeManagerAPI to be aboutAPI

--- src/main/java/org/elastos/hive/network/AboutAPI.java
+++ src/main/java/org/elastos/hive/network/AboutAPI.java
@@ -5,10 +5,10 @@ import org.elastos.hive.network.response.NodeVersionResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface NodeManageApi {
-	@GET(BaseApi.API_VERSION + "/hive/version")
+public interface AboutAPI {
+	@GET(BaseApi.API_VERSION + "/about/version")
 	Call<NodeVersionResponseBody> version();
 
-	@GET(BaseApi.API_VERSION + "/hive/commithash")
+	@GET(BaseApi.API_VERSION + "/about/commithash")
 	Call<NodeCommitHashResponseBody> commitHash();
 }
