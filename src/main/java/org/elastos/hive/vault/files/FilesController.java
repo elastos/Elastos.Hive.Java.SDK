4ca884ce6ca08fcfc283e5c1d564edb7e6f0bacc
stiartsly <stiartsly@gmail.com>
2021-05-26 10:56:29 +0800

    CU-8tvure - Improve the internal invocation to RPC APIs for files module

--- src/main/java/org/elastos/hive/vault/files/FilesController.java
+++ src/main/java/org/elastos/hive/vault/files/FilesController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.elastos.hive.ServiceEndpoint;
 import org.elastos.hive.connection.ConnectionManager;
 import org.elastos.hive.connection.HiveResponseBody;
+import org.elastos.hive.exception.HiveException;
 
 public class FilesController {
 	private FilesAPI filesAPI;
@@ -16,37 +17,82 @@ public class FilesController {
 		connectionManager = serviceEndpoint.getConnectionManager();
 	}
 
-	public List<FileInfo> listChildren(String path) throws IOException {
-		return HiveResponseBody.validateBody(filesAPI.listChildren(path).execute().body()).getFileInfoList();
-	}
-
-	public void copyFile(String srcPath, String dstPath) throws IOException {
-		HiveResponseBody.validateBody(filesAPI.copy(new FilesCopyRequestBody(srcPath, dstPath)).execute());
-	}
-
-	public void moveFile(String srcPath, String dstPath) throws IOException {
-		HiveResponseBody.validateBody(filesAPI.move(new FilesMoveRequestBody(srcPath, dstPath)).execute());
-	}
-
-	public void delete(String path) throws IOException {
-		HiveResponseBody.validateBody(filesAPI.delete(new FilesDeleteRequestBody(path)).execute());
-	}
-
-	public FileInfo getProperty(String path) throws IOException {
-		return HiveResponseBody.validateBody(filesAPI.properties(path).execute().body()).getFileInfo();
-	}
-
-	public String getHash(String path) throws IOException {
-		return HiveResponseBody.validateBody(filesAPI.hash(path).execute().body()).getSha256();
-	}
-
-	public <T> T upload(String path, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getRequestStream(
+	public <T> T upload(String path, Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getRequestStream(
 				connectionManager.openConnection(FilesAPI.API_UPLOAD + "/" + path),
 				resultType);
+		} catch (IOException e) {
+			// TODO:
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	public <T> T download(String path, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getResponseStream(filesAPI.download(path).execute(), resultType);
+	public <T> T download(String path, Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getResponseStream(filesAPI.download(path).execute(), resultType);
+		} catch (IOException e) {
+			// TODO:
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public List<FileInfo> listChildren(String path) throws HiveException {
+		try {
+			return filesAPI.listChildren(path).execute().body().getFileInfoList();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void copyFile(String srcPath, String destPath) throws HiveException {
+		try {
+			filesAPI.copy(new FilesCopyRequestBody(srcPath, destPath)).execute();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+	}
+
+	public void moveFile(String srcPath, String destPath) throws HiveException {
+		try {
+			filesAPI.move(new FilesMoveRequestBody(srcPath, destPath)).execute();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(String path) throws HiveException {
+		try {
+			filesAPI.delete(new FilesDeleteRequestBody(path)).execute();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+	}
+
+	public FileInfo getProperty(String path) throws HiveException {
+		try {
+			return filesAPI.properties(path).execute().body().getFileInfo();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String getHash(String path) throws HiveException {
+		try {
+			return filesAPI.hash(path).execute().body().getSha256();
+		} catch (IOException e) {
+			// TODO: throw exception.
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
