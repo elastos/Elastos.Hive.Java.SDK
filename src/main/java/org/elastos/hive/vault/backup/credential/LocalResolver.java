33dc2b321ea75f20ea54a51a0685fe09d330a5c3
stiartsly <stiartsly@gmail.com>
2021-05-27 21:41:57 +0800

    CU-8tvure - organize concrete CodeResover for backup credenital

---
+++ src/main/java/org/elastos/hive/vault/backup/credential/LocalResolver.java
@@ -0,0 +1,51 @@
+package org.elastos.hive.vault.backup.credential;
+
+import org.elastos.hive.ServiceEndpoint;
+import org.elastos.hive.auth.CodeResolver;
+import org.elastos.hive.exception.HttpFailedException;
+import org.elastos.hive.storage.DataStorage;
+
+public class LocalResolver implements CodeResolver {
+	private ServiceEndpoint serviceEndpoint;
+	private CodeResolver nextResolver;
+	private DataStorage storage;
+
+    public LocalResolver(ServiceEndpoint serviceEndpoint, CodeResolver next) {
+    	this.serviceEndpoint = serviceEndpoint;
+    	this.nextResolver = next;
+    	this.storage = serviceEndpoint.getAppContext().getDataStorage();
+    }
+
+    @Override
+	public String resolve() throws HttpFailedException {
+		String token = restoreToken();
+		if (token == null) {
+			token = nextResolver.resolve();
+			saveToken(token);
+		}
+
+		return token;
+	}
+
+	@Override
+	public void invalidate() {
+		clearToken();
+	}
+
+	private String restoreToken() {
+        if (serviceEndpoint.getServiceInstanceDid() == null)
+            return null;
+
+        return storage.loadBackupCredential(serviceEndpoint.getServiceInstanceDid());
+    }
+
+    private void saveToken(String token) {
+        if (serviceEndpoint.getServiceInstanceDid() != null)
+        	storage.storeBackupCredential(serviceEndpoint.getServiceInstanceDid(), token);
+    }
+
+    private void clearToken() {
+        if (serviceEndpoint.getServiceInstanceDid() != null)
+        	storage.clearBackupCredential(serviceEndpoint.getServiceInstanceDid());
+    }
+}
