7981073ecac571aae60c06def9bc72f8b874d0b5
Fred Chen <chentj070921@yeah.net>
2021-05-27 15:00:17 +0800

    CU-8u0gnd - Refine the files module.

--- src/main/java/org/elastos/hive/vault/files/FilesDeleteRequestBody.java
+++
@@ -1,9 +0,0 @@
-package org.elastos.hive.vault.files;
-
-class FilesDeleteRequestBody {
-    private final String path;
-
-    public FilesDeleteRequestBody(String path) {
-        this.path = path;
-    }
-}
