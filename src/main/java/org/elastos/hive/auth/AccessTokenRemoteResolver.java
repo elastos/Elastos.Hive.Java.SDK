f0da0314f6a8220f7e528adeadd419ff399a979a
stiartsly <stiartsly@gmail.com>
2021-05-27 15:34:02 +0800

    CU-8tvure - improve internal implementation of AccessTokenRemoteResolver

--- src/main/java/org/elastos/hive/auth/AccessTokenRemoteResolver.java
+++ src/main/java/org/elastos/hive/auth/AccessTokenRemoteResolver.java
@@ -27,30 +27,21 @@ public class AccessTokenRemoteResolver implements CodeResolver {
 	@Override
 	public String resolve() throws HttpFailedException {
 		try {
-			return controller.auth(signIn4AccessToken());
-	        /*long exp = JwtUtil.getBody(accessToken).getExpiration().getTime();
-	        long expiresTime = System.currentTimeMillis() / 1000 + exp / 1000;
-	        return new AuthTokenToVault(accessToken, expiresTime);
-	        */
+			String challenge = controller.signIn(contextProvider.getAppInstanceDocument().toString());
+			Claims claims = JwtUtil.getBody(challenge);
+	        // Update the service did to service end-point for future usage.
+	        serviceEndpoint.setServiceInstanceDid(claims.getIssuer());
+	        serviceEndpoint.setAppInstanceDid(claims.getSubject());
 
+	        String challengeResponse = contextProvider.getAuthorization(challenge).get();
+	        return controller.auth(challengeResponse);
 		} catch (Exception e) {
+			// TODO:
+			e.printStackTrace();
 			LogUtil.d(Throwables.getStackTraceAsString(e));
 			throw new HttpFailedException(401, "Failed to get token by auth requests.");
 		}
 	}
-
-	public String signIn() throws HiveException {
-        String challenge = controller.signIn(contextProvider.getAppInstanceDocument().toString());
-        Claims claims = JwtUtil.getBody(challenge);
-        // Update the service did to service end-point for future usage.
-        serviceEndpoint.setServiceInstanceDid(claims.getIssuer());
-        serviceEndpoint.setAppInstanceDid(claims.getSubject());
-        return challenge;
-    }
-
-	private String signIn4AccessToken() throws HiveException, ExecutionException, InterruptedException {
-        return contextProvider.getAuthorization(signIn()).get();
-    }
 
 	@Override
 	public void invalidate() {}
