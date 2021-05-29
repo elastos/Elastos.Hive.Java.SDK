38573fb5e3d85dc1cb9579cc1a8281a6943b95ad
stiartsly <stiartsly@gmail.com>
2021-05-29 17:07:10 +0800

    CU-8tvure - Format UploadOutputStream and UploadOutputStreamWriter class defintion

--- src/main/java/org/elastos/hive/connection/UploadOutputStreamWriter.java
+++ src/main/java/org/elastos/hive/connection/UploadOutputStreamWriter.java
@@ -1,7 +1,5 @@
 package org.elastos.hive.connection;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -10,8 +8,8 @@ import java.net.HttpURLConnection;
 public class UploadOutputStreamWriter extends OutputStreamWriter {
     private final HttpURLConnection connection;
 
-    public UploadOutputStreamWriter(HttpURLConnection connection, @NotNull OutputStream out) {
-        super(out);
+    public UploadOutputStreamWriter(HttpURLConnection connection, OutputStream output) {
+        super(output);
         this.connection = connection;
     }
 
@@ -19,6 +17,6 @@ public class UploadOutputStreamWriter extends OutputStreamWriter {
     public void close() throws IOException {
         super.close();
         // In order to get the failed message from response body.
-        ConnectionManager.readConnection(connection);
+        new ConnectionClosure().confirmClosed(connection);
     }
 }
