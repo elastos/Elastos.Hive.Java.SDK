ae637301316400174166ea9f69546865c7103a44
stiartsly <stiartsly@gmail.com>
2021-05-29 14:47:22 +0800

    Use tab to replace 4 whitespaces

--- src/main/java/org/elastos/hive/vault/files/HashInfo.java
+++ src/main/java/org/elastos/hive/vault/files/HashInfo.java
@@ -3,24 +3,24 @@ package org.elastos.hive.vault.files;
 import com.google.gson.annotations.SerializedName;
 
 class HashInfo {
-    @SerializedName("name")
-    private String name;
+	@SerializedName("name")
+	private String name;
 
-    @SerializedName("algorithm")
-    private String algorithm;
+	@SerializedName("algorithm")
+	private String algorithm;
 
-    @SerializedName("hash")
-    private String hash;
+	@SerializedName("hash")
+	private String hash;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getAlgorithm() {
-        return algorithm;
-    }
+	public String getAlgorithm() {
+		return algorithm;
+	}
 
-    public String getHash() {
-        return hash;
-    }
+	public String getHash() {
+		return hash;
+	}
 }
