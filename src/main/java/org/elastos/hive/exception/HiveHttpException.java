06db84dc98f67cb474d7f8ec4eaef077158e36ce
Fred Chen <chentj070921@yeah.net>
2021-05-31 12:21:42 +0800

    CU-8u0gnd - Update exception handling for subscribe/unsubscribe in vault subscription module.

--- src/main/java/org/elastos/hive/exception/HiveHttpException.java
+++ src/main/java/org/elastos/hive/exception/HiveHttpException.java
@@ -2,17 +2,26 @@ package org.elastos.hive.exception;
 
 import java.io.IOException;
 
+/**
+ * Main error response exception for internal usage.
+ */
 public class HiveHttpException extends IOException {
     private static final long serialVersionUID = 1L;
 
     private final int code;
+    private final int internalCode;
 
-    public HiveHttpException(int code, String message) {
+    public HiveHttpException(int code, int internalCode, String message) {
         super(message);
         this.code = code;
+        this.internalCode = internalCode;
     }
 
     public int getCode() {
         return code;
+    }
+
+    public int getInternalCode() {
+        return internalCode;
     }
 }
