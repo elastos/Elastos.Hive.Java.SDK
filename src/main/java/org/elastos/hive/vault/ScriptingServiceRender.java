75e4493607581881b9020e4a082c110159cccfaa
stiartsly <stiartsly@gmail.com>
2021-06-08 13:49:03 +0800

    CU-8tvure - Canonicalize scripting implementation

--- src/main/java/org/elastos/hive/vault/ScriptingServiceRender.java
+++ src/main/java/org/elastos/hive/vault/ScriptingServiceRender.java
@@ -2,7 +2,7 @@ package org.elastos.hive.vault;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.elastos.hive.ServiceEndpoint;
-import org.elastos.hive.exception.InvalidParameterV1Exception;
+import org.elastos.hive.exception.HiveException;
 import org.elastos.hive.exception.NotImplementedException;
 import org.elastos.hive.vault.scripting.Condition;
 import org.elastos.hive.vault.scripting.Executable;
@@ -21,29 +21,40 @@ public class ScriptingServiceRender implements ScriptingService {
 
 	@Override
 	public CompletableFuture<Void> registerScript(String name, Executable executable,
-												  boolean allowAnonymousUser, boolean allowAnonymousApp) {
+												boolean allowAnonymousUser,
+												boolean allowAnonymousApp) {
 		return registerScript(name, null, executable, allowAnonymousUser, allowAnonymousApp);
 	}
 
 	@Override
 	public CompletableFuture<Void> registerScript(String name,
-												  Condition condition, Executable executable,
-												  boolean allowAnonymousUser, boolean allowAnonymousApp) {
+												Condition condition, Executable executable,
+												boolean allowAnonymousUser,
+												boolean allowAnonymousApp) {
 		return CompletableFuture.runAsync(()-> {
 			try {
 				controller.registerScript(name, condition, executable, allowAnonymousUser, allowAnonymousApp);
-			} catch (Exception e) {
+			} catch (HiveException | RuntimeException e) {
 				throw new CompletionException(e);
 			}
 		});
 	}
 
 	@Override
-	public <T> CompletableFuture<T> callScript(String name, JsonNode params, String targetDid, String targetAppDid, Class<T> resultType) {
+	public CompletableFuture<Void> unregisterScript(String name) {
+		// TODO:
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public <T> CompletableFuture<T> callScript(String name, JsonNode params,
+												String targetDid,
+												String targetAppDid,
+												Class<T> resultType) {
 		return CompletableFuture.supplyAsync(()-> {
 			try {
 				return controller.callScript(name, params, targetDid, targetAppDid, resultType);
-			} catch (Exception e) {
+			} catch (HiveException | RuntimeException e) {
 				throw new CompletionException(e);
 			}
 		});
@@ -53,7 +64,7 @@ public class ScriptingServiceRender implements ScriptingService {
 		return CompletableFuture.supplyAsync(()-> {
 			try {
 				return controller.callScriptUrl(name, params, targetDid, targetAppDid, resultType);
-			} catch (Exception e) {
+			} catch (HiveException | RuntimeException e) {
 				throw new CompletionException(e);
 			}
 		});
@@ -64,10 +75,10 @@ public class ScriptingServiceRender implements ScriptingService {
 		return CompletableFuture.supplyAsync(()-> {
 			try {
 				if (transactionId == null)
-					throw new InvalidParameterV1Exception("Invalid parameter transactionId.");
+					throw new IllegalArgumentException("Invalid parameter transactionId.");
 
 				return controller.uploadFile(transactionId, resultType);
-			} catch (Exception e) {
+			} catch (HiveException | RuntimeException e) {
 				throw new CompletionException(e);
 			}
 		});
@@ -78,17 +89,12 @@ public class ScriptingServiceRender implements ScriptingService {
 		return CompletableFuture.supplyAsync(()-> {
 			try {
 				if (transactionId == null)
-					throw new InvalidParameterV1Exception("Invalid parameter transactionId.");
+					throw new IllegalArgumentException("Invalid parameter transactionId.");
 
 				return controller.downloadFile(transactionId, resultType);
-			} catch (Exception e) {
+			} catch (HiveException | RuntimeException e) {
 				throw new CompletionException(e);
 			}
 		});
-	}
-
-	@Override
-	public CompletableFuture<Void> unregisterScript(String name) {
-		throw new NotImplementedException();
 	}
 }
