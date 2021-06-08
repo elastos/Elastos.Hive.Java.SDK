5a4dff56e72fe5d5b8874e786ecb45e9cfa7edad
stiartsly <stiartsly@gmail.com>
2021-06-08 13:49:03 +0800

    CU-8tvure - Canonicalize scripting implementation

--- src/main/java/org/elastos/hive/vault/scripting/ScriptingController.java
+++ src/main/java/org/elastos/hive/vault/scripting/ScriptingController.java
@@ -1,9 +1,12 @@
 package org.elastos.hive.vault.scripting;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.elastos.hive.ServiceEndpoint;
 import org.elastos.hive.connection.ConnectionManager;
 import org.elastos.hive.connection.HiveResponseBody;
+import org.elastos.hive.exception.HiveException;
+import org.elastos.hive.exception.NetworkException;
+import org.elastos.hive.exception.RPCException;
+import org.elastos.hive.exception.UnknownServerException;
 
 import java.io.IOException;
 
@@ -17,39 +20,90 @@ public class ScriptingController {
 		this.scriptingAPI = connection.createService(ScriptingAPI.class);
 	}
 
-	public void registerScript(String name, Condition condition, Executable executable,
-							   boolean allowAnonymousUser, boolean allowAnonymousApp) throws IOException {
-		scriptingAPI.registerScript(name, new RegisterScriptRequest()
-						.setExecutable(executable)
-						.setAllowAnonymousUser(allowAnonymousUser)
-						.setAllowAnonymousApp(allowAnonymousApp)
-						.setCondition(condition))
-						.execute().body();
+	public void registerScript(String name,
+							Condition condition, Executable executable,
+							boolean allowAnonymousUser,
+							boolean allowAnonymousApp) throws HiveException {
+		try {
+			scriptingAPI.registerScript(name, new RegisterScriptRequest()
+							.setExecutable(executable)
+							.setAllowAnonymousUser(allowAnonymousUser)
+							.setAllowAnonymousApp(allowAnonymousApp)
+							.setCondition(condition))
+							.execute().body();
+		} catch (RPCException e) {
+			e.printStackTrace();
+			// TODO:
+		} catch (IOException e) {
+			throw new NetworkException(e);
+		}
 	}
 
 	public <T> T callScript(String name, JsonNode params,
-							String targetDid, String targetAppDid, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getValue(HiveResponseBody.validateBodyStr(
-				scriptingAPI.runScript(name, new CallScriptRequest()
-					.setContext(new ScriptContext().setTargetDid(targetDid).setTargetAppDid(targetAppDid))
-					.setParams(HiveResponseBody.jsonNode2Map(params)))
-					.execute(), false), resultType);
+							String targetDid,
+							String targetAppDid,
+							Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getValue(HiveResponseBody.validateBodyStr(
+					scriptingAPI.runScript(name, new CallScriptRequest()
+							.setContext(new ScriptContext()
+							.setTargetDid(targetDid)
+							.setTargetAppDid(targetAppDid))
+							.setParams(HiveResponseBody.jsonNode2Map(params)))
+							.execute(), false), resultType);
+		} catch (RPCException e) {
+			// TODO:
+			throw new UnknownServerException(e);
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+			throw new NetworkException(e);
+		}
 	}
 
 	public <T> T callScriptUrl(String name, String params,
-							   String targetDid, String targetAppDid, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getValue(HiveResponseBody.validateBodyStr(
-				scriptingAPI.runScriptUrl(name, targetDid, targetAppDid, params)
-						.execute(), false), resultType);
+							   String targetDid,
+							   String targetAppDid,
+							   Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getValue(HiveResponseBody.validateBodyStr(
+					scriptingAPI.runScriptUrl(name, targetDid, targetAppDid, params)
+							.execute(), false), resultType);
+		} catch (RPCException e) {
+			// TODO:
+			throw new UnknownServerException(e);
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+			throw new NetworkException(e);
+		}
 	}
 
-	public <T> T uploadFile(String transactionId, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getRequestStream(
-				connectionManager.openConnection(ScriptingAPI.API_SCRIPT_UPLOAD + "/" + transactionId),
-				resultType);
+	public <T> T uploadFile(String transactionId, Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getRequestStream(
+					connectionManager.openConnection(ScriptingAPI.API_SCRIPT_UPLOAD + "/" + transactionId),
+					resultType);
+		} catch (RPCException e) {
+			// TODO:
+			throw new UnknownServerException(e);
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+			throw new NetworkException(e);
+		}
 	}
 
-	public <T> T downloadFile(String transactionId, Class<T> resultType) throws IOException {
-		return HiveResponseBody.getResponseStream(scriptingAPI.downloadFile(transactionId).execute(), resultType);
+	public <T> T downloadFile(String transactionId, Class<T> resultType) throws HiveException {
+		try {
+			return HiveResponseBody.getResponseStream(scriptingAPI.downloadFile(transactionId).execute(), resultType);
+		} catch (RPCException e) {
+			// TODO:
+			throw new UnknownServerException(e);
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+			throw new NetworkException(e);
+		}
 	}
 }
