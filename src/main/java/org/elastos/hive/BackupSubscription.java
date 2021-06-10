fe49f420f5282eb0b3e916e6ee90629bede7d643
stiartsly <stiartsly@gmail.com>
2021-06-10 15:14:39 +0800

    CU-8tvure - Unimplementing payment methods for vault/backup subscription serviceEndpoint

--- src/main/java/org/elastos/hive/BackupSubscription.java
+++ src/main/java/org/elastos/hive/BackupSubscription.java
@@ -5,27 +5,23 @@ import org.elastos.hive.exception.NotImplementedException;
 import org.elastos.hive.subscription.BackupInfo;
 import org.elastos.hive.subscription.PricingPlan;
 import org.elastos.hive.subscription.payment.Order;
-import org.elastos.hive.subscription.payment.PaymentController;
 import org.elastos.hive.subscription.payment.Receipt;
 import org.elastos.hive.service.PaymentService;
 import org.elastos.hive.service.SubscriptionService;
 import org.elastos.hive.subscription.SubscriptionController;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
 public class BackupSubscription extends ServiceEndpoint
-	implements SubscriptionService<BackupInfo>, PaymentService {
+		implements SubscriptionService<BackupInfo>, PaymentService {
 
 	private SubscriptionController subscriptionController;
-	private PaymentController paymentController;
 
 	public BackupSubscription(AppContext context, String providerAddress) throws HiveException {
 		super(context, providerAddress);
 		subscriptionController = new SubscriptionController(this.getConnectionManager());
-		paymentController = new PaymentController(this);
 	}
 
 	@Override
@@ -42,6 +38,9 @@ public class BackupSubscription extends ServiceEndpoint
 	@Override
 	public CompletableFuture<PricingPlan> getPricingPlan(String planName) {
 		return CompletableFuture.supplyAsync(()-> {
+			if (planName == null)
+				throw new IllegalArgumentException("Empty plan name");
+
 			try {
 				return subscriptionController.getBackupPricingPlan(planName);
 			} catch (RuntimeException | HiveException e) {
@@ -55,8 +54,11 @@ public class BackupSubscription extends ServiceEndpoint
 	}
 
 	@Override
-	public CompletableFuture<BackupInfo> subscribe(String reserved) {
+	public CompletableFuture<BackupInfo> subscribe(String credential) {
 		return CompletableFuture.supplyAsync(()-> {
+			if (credential != null)
+				throw new NotImplementedException("Paid pricing plan will be supported later");
+
 			try {
 				return subscriptionController.subscribeToBackup(null);
 			} catch (HiveException | RuntimeException e) {
@@ -89,50 +91,43 @@ public class BackupSubscription extends ServiceEndpoint
 
 	@Override
 	public CompletableFuture<Order> placeOrder(String planName) {
-		return CompletableFuture.supplyAsync(()-> {
-			try {
-				return paymentController.getOrderInfo(paymentController.createOrder(null, planName));
-			} catch (HiveException | RuntimeException e) {
-				throw new CompletionException(e);
-			}
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
 		});
 	}
 
 	@Override
 	public CompletableFuture<Order> getOrder(String orderId) {
-		return CompletableFuture.supplyAsync(()-> {
-			try {
-				return paymentController.getOrderInfo(orderId);
-			} catch (HiveException | RuntimeException e) {
-				throw new CompletionException(e);
-			}
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
 		});
 	}
 
 	@Override
 	public CompletableFuture<Receipt> payOrder(String orderId, String transactionId) {
-		return CompletableFuture.supplyAsync(()-> {
-			try {
-				paymentController.payOrder(orderId, Collections.singletonList(transactionId));
-				return null;
-			} catch (HiveException | RuntimeException e) {
-				throw new CompletionException(e);
-			}
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
 		});
 	}
 
 	@Override
 	public CompletableFuture<Receipt> getReceipt(String receiptId) {
-		throw new NotImplementedException();
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
+		});
 	}
 
 	@Override
 	public CompletableFuture<List<Order>> getOrderList() {
-		throw new NotImplementedException();
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
+		});
 	}
 
 	@Override
 	public CompletableFuture<List<Receipt>> getReceiptList() {
-		throw new NotImplementedException();
+		return CompletableFuture.supplyAsync(() -> {
+			throw new NotImplementedException("Payment will be supported later");
+		});
 	}
 }
