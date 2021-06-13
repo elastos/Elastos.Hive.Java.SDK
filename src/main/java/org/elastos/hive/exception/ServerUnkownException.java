135e29fee0c9d3423f55a034efb14687ac9846cd
stiartsly <stiartsly@gmail.com>
2021-06-13 11:20:47 +0800

    update internal implementation for AuthController class

--- src/main/java/org/elastos/hive/exception/ServerUnkownException.java
+++ src/main/java/org/elastos/hive/exception/ServerUnkownException.java
@@ -5,12 +5,16 @@ import org.elastos.hive.connection.NodeRPCException;
 public class ServerUnkownException extends HiveException {
 	private static final long serialVersionUID = 5210865275817148567L;
 
+	public ServerUnkownException() {
+		super("Impossible failure happened");
+	}
+
 	public ServerUnkownException(String message) {
-		super(String.format("Unkown IO exception with message: %s", message));
+		super(message);
 	}
 
 	public ServerUnkownException(int httpCode, String message) {
-		super(String.format("Impossible Exception with HTTP code:%d and message: %s", httpCode, message));
+		super(String.format("Exception (http code: %d, message: %s)", httpCode, message));
 	}
 
 	public ServerUnkownException(NodeRPCException e) {
