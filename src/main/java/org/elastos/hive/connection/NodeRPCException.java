336c20f88543207d06b0741a6d83c7dc2755242e
Fred Chen <chentj070921@yeah.net>
2021-06-22 11:23:03 +0800

    CU-9hw9xv - Refine the backup module exceptions.

--- src/main/java/org/elastos/hive/connection/NodeRPCException.java
+++ src/main/java/org/elastos/hive/connection/NodeRPCException.java
@@ -8,10 +8,16 @@ import java.io.IOException;
 public class NodeRPCException extends IOException {
 	private static final long serialVersionUID = 1L;
 
-	public static final int BAD_REQUEST	    = 400;
-	public static final int UNAUTHORIZED	= 401;
-	public static final int NOT_FOUND	    = 404;
-	public static final int ALREADY_EXISTS  = 455;
+	public static final int BAD_REQUEST	    		= 400;
+	public static final int UNAUTHORIZED			= 401;
+	public static final int FORBIDDEN	    		= 403;
+	public static final int NOT_FOUND	    		= 404;
+	public static final int ALREADY_EXISTS  		= 455;
+	public static final int INSUFFICIENT_STORAGE  	= 507;
+
+	// BAD_REQUEST
+	public static final int IC_INVALID_PARAMETER  		= 1;
+	public static final int IC_BACKUP_IS_IN_PROCESSING  = 2;
 
 	private final int code;
 	private final int internalCode;
