35edc4ae385c58b85a4240dda90a7679a8557dc8
Fred Chen <chentj070921@yeah.net>
2021-08-26 18:46:52 +0800

    CU-f7xha2 - Add test cases for the about module.

---
+++ src/test/java/org/elastos/hive/AboutTest.java
@@ -0,0 +1,34 @@
+package org.elastos.hive;
+
+import org.elastos.hive.config.TestData;
+import org.elastos.hive.endpoint.NodeVersion;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AboutTest {
+	private static VaultSubscription subscription;
+
+	@BeforeAll public static void setup() {
+		Assertions.assertDoesNotThrow(()->{
+			TestData testData = TestData.getInstance();
+			subscription = new VaultSubscription(
+					testData.getAppContext(),
+					testData.getProviderAddress());
+		});
+	}
+
+	@Test @Order(1) void testGetVersion() {
+		Assertions.assertDoesNotThrow(()->{
+			NodeVersion version = subscription.getNodeVersion().get();
+			Assertions.assertNotNull(version);
+			Assertions.assertTrue(version.major() > 0);
+		});
+	}
+
+	@Test @Order(2) void testGetCommitId() {
+		Assertions.assertDoesNotThrow(()-> {
+			String commitId = subscription.getLatestCommitId().get();
+			Assertions.assertNotNull(commitId);
+		});
+	}
+}
