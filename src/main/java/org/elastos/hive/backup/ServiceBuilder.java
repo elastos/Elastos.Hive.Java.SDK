e9b8da17c8c7bcdcaf4f820c5f5ee4ab3f941e36
Fred Chen <chentj070921@yeah.net>
2021-12-29 11:42:36 +0800

    CU-1zpp3rm - Add the API docs of the provider service.

--- src/main/java/org/elastos/hive/backup/ServiceBuilder.java
+++ src/main/java/org/elastos/hive/backup/ServiceBuilder.java
@@ -6,7 +6,6 @@ import org.elastos.hive.service.PromotionService;
 /**
  * The service builder for the services of the backup hive node.
  *
- * <p>TODO: to be implemented.</p>
  */
 public class ServiceBuilder {
 	private ServiceEndpoint backup;
